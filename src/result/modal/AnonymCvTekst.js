7bfb2f3f59660e84273da6a374ee7dc99fc09f23
Tony Gjendahl <tony.gjendahl@nav.no>
2018-06-12 13:49:46 +0200

    Legge til feature-toggle på send beskjed kandidat i anonym cv

--- src/result/modal/AnonymCvTekst.js
+++ src/result/modal/AnonymCvTekst.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Knapp } from 'nav-frontend-knapper';
 import { Row } from 'nav-frontend-grid';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import './Modal.less';
 
-export default function AnonymCvTekst({ onTaKontaktClick, toggleModalOpen }) {
+function AnonymCvTekst({ onTaKontaktClick, toggleModalOpen, visTaKontaktKandidat }) {
     return (
         <div className="panel panel--padding">
             <Row>
@@ -16,27 +17,40 @@ export default function AnonymCvTekst({ onTaKontaktClick, toggleModalOpen }) {
                     kontaktinformasjon til kandidaten slik at personen kan kontakte deg.
                 </Normaltekst>
             </Row>
-            <Row>
-                <div className="row cv--button--row">
-                    <Knapp
-                        className="knapp knapp--hoved"
-                        onClick={onTaKontaktClick}
-                    >
-                        Kontakt kandidat
-                    </Knapp>
-                    <Knapp
-                        className="knapp"
-                        onClick={toggleModalOpen}
-                    >
-                        Avbryt
-                    </Knapp>
-                </div>
-            </Row>
+            {visTaKontaktKandidat && (
+                <Row>
+                    <div className="row cv--button--row">
+                        <Knapp
+                            className="knapp knapp--hoved"
+                            onClick={onTaKontaktClick}
+                        >
+                            Kontakt kandidat
+                        </Knapp>
+                        <Knapp
+                            className="knapp"
+                            onClick={toggleModalOpen}
+                        >
+                            Avbryt
+                        </Knapp>
+                    </div>
+                </Row>
+            )}
         </div>
     );
 }
 
+AnonymCvTekst.defaultProps = {
+    visTaKontaktKandidat: false
+};
+
 AnonymCvTekst.propTypes = {
     onTaKontaktClick: PropTypes.func.isRequired,
-    toggleModalOpen: PropTypes.func.isRequired
+    toggleModalOpen: PropTypes.func.isRequired,
+    visTaKontaktKandidat: PropTypes.bool
 };
+
+const mapStateToProps = (state) => ({
+    visTaKontaktKandidat: state.search.featureToggles['vis-ta-kontakt-kandidat']
+});
+
+export default connect(mapStateToProps)(AnonymCvTekst);
