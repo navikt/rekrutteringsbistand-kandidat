9ea9bfc2ecc696f82cd199eebefc503175fd7af0
joar <joar.aurdal@gmail.com>
2018-09-10 12:51:02 +0200

    feature/pam-1197_searchmatch

--- src/sok/api.js
+++ src/sok/api.js
@@ -75,12 +75,6 @@ export function fetchKandidater(query = {}) {
     );
 }
 
-export function fetchKandidaterCount(query = {}) {
-    return fetchJson(
-        `${SEARCH_API}sok/count?${convertToUrlParams(query)}`, true
-    );
-}
-
 export function fetchCv(arenaKandidatnr) {
     return fetchJson(
         `${SEARCH_API}hentcv?${convertToUrlParams(arenaKandidatnr)}`, true
