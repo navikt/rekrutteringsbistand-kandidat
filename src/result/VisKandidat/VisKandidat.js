d258209f0bcc33867aa537d6b597e11627bbd262
Eirik Vilpponen <eirik.vilpponen@nav.no>
2018-09-20 14:16:20 +0200

    PAM-1263 Info om kandidaten. Kandidat personalia. Enkel link tilbake til '/pam-kandidatsok'

--- src/result/VisKandidat/VisKandidat.js
+++ src/result/VisKandidat/VisKandidat.js
@@ -2,9 +2,9 @@ import React from 'react';
 import connect from 'react-redux/es/connect/connect';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 import PropTypes from 'prop-types';
-import cvPropTypes from '../../PropTypes';
+import { cvPropTypes } from '../../PropTypes';
 import { MatchexplainProptypesGrouped } from '../modal/Proptypes';
-import { FETCH_CV, OPEN_CV_MODAL } from '../../sok/cv/cvReducer';
+import { FETCH_CV } from '../../sok/cv/cvReducer';
 import { getUrlParameterByName } from '../../sok/utils';
 import VisKandidatPersonalia from './VisKandidatPersonalia';
 import VisKandidatCv from './VisKandidatCv';
@@ -17,7 +17,6 @@ class VisKandidat extends React.Component {
 
     render() {
         const { cv, isFetchingCv, matchforklaring } = this.props;
-
 
         if (isFetchingCv) {
             return (
@@ -55,7 +54,6 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    openCvModal: () => dispatch({ type: OPEN_CV_MODAL }),
     hentCvForKandidat: (arenaKandidatnr) => dispatch({ type: FETCH_CV, arenaKandidatnr })
 });
 
--- src/result/VisKandidat/VisKandidatPersonalia.js
+++ src/result/VisKandidat/VisKandidatPersonalia.js
@@ -1,53 +1,65 @@
 import { Column, Row } from 'nav-frontend-grid';
 import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
 import React from 'react';
+import connect from 'react-redux/es/connect/connect';
+import { Link } from 'react-router-dom';
 import { formatISOString } from '../../common/dateUtils';
-import cvPropTypes from '../../PropTypes';
+import { cvPropTypes } from '../../PropTypes';
 
-const VisKandidatPersonalia = ({ cv }) => (
-    <div>
-        <Row className="blokk-s personalia--modal">
-            <Column xs="12">
-                <Sidetittel className="navn--modal">
-                    {cv.fornavn} {cv.etternavn}
-                </Sidetittel>
-                {cv.fodselsdato && (
-                    <Normaltekst><strong>Fødselsdato:</strong> {formatISOString(cv.fodselsdato, 'D. MMMM YYYY')}</Normaltekst>
-                )}
-                {cv.adresse && cv.adresse.adrlinje1 && (
-                    <Normaltekst>
-                        <strong>Adresse:</strong> {cv.adresse.adrlinje1}
-                        {(cv.adresse.postnr || cv.adresse.poststednavn) ?
-                            (', ') : null}
-                        {cv.adresse.postnr} {cv.adresse.poststednavn}
-                    </Normaltekst>
-                )}
-                {(cv.epost || cv.telefon) && (
-                    <div className="kontakt--modal">
-                        {cv.epost && (
+class VisKandidatPersonalia extends React.Component {
+    render() {
+        const cv = this.props.cv;
+        return (
+            <div>
+                <Row className="blokk-s personalia--modal">
+                    <Column xs="12">
+                        <Link
+                            to={'/pam-kandidatsok'}
+                        >
+                            Tilbake til sok
+                        </Link>
+                        <Sidetittel className="navn--modal">
+                            {cv.fornavn} {cv.etternavn}
+                        </Sidetittel>
+                        {cv.fodselsdato && (
+                            <Normaltekst><strong>Fødselsdato:</strong> {formatISOString(cv.fodselsdato, 'D. MMMM YYYY')}</Normaltekst>
+                        )}
+                        {cv.adresse && cv.adresse.adrlinje1 && (
                             <Normaltekst>
-                                <i className="mail--icon" />
-                                <strong>E-post:</strong>
-                                <a href={`mailto:${cv.epost}`} className="lenke mail--text">{cv.epost}</a>
+                                <strong>Adresse:</strong> {cv.adresse.adrlinje1}
+                                {(cv.adresse.postnr || cv.adresse.poststednavn) ?
+                                    (', ') : null}
+                                {cv.adresse.postnr} {cv.adresse.poststednavn}
                             </Normaltekst>
                         )}
-                        {cv.telefon && (
-                            <Normaltekst>
-                                <i className="telefon--icon" />
-                                <strong>Telefon:</strong> {/* TODO: Telefon er ikke med
+                        {(cv.epost || cv.telefon) && (
+                            <div className="kontakt--modal">
+                                {cv.epost && (
+                                    <Normaltekst>
+                                        <i className="mail--icon" />
+                                        <strong>E-post:</strong>
+                                        <a href={`mailto:${cv.epost}`} className="lenke mail--text">{cv.epost}</a>
+                                    </Normaltekst>
+                                )}
+                                {cv.telefon && (
+                                    <Normaltekst>
+                                        <i className="telefon--icon" />
+                                        <strong>Telefon:</strong> {/* TODO: Telefon er ikke med
                                     fra backend per nå, oppdatere denne når det er med */}
-                                {/* <a href={`tel:${cv.telefon}`} className="lenke telefon--text">{cv.telefon}</a> */}
-                            </Normaltekst>
+                                        {/* <a href={`tel:${cv.telefon}`} className="lenke telefon--text">{cv.telefon}</a> */}
+                                    </Normaltekst>
+                                )}
+                            </div>
                         )}
-                    </div>
-                )}
-            </Column>
-        </Row>
-    </div>
-);
+                    </Column>
+                </Row>
+            </div>
+        );
+    }
+}
 
 VisKandidatPersonalia.propTypes = {
     cv: cvPropTypes.isRequired
 };
 
-export default VisKandidatPersonalia;
+export default connect()(VisKandidatPersonalia);
