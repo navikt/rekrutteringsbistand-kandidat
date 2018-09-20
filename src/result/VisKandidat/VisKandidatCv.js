ede98d32fabd84b603f0ee501bea1de20bb8702c
erlendjohansen <erlend.johansen@nav.no>
2018-09-20 14:47:44 +0200

    PAM-1429 Vis CV til kandidaten. Fjerner konstanter som kopierer arrayene fra VisKandidatCv konstanten og returnerer konstanten direkte.

--- src/result/VisKandidat/VisKandidatCv.js
+++ src/result/VisKandidat/VisKandidatCv.js
@@ -7,164 +7,156 @@ import sortByDato from '../../common/SortByDato';
 import Tidsperiode from '../../common/Tidsperiode';
 import './VisKandidat.less';
 
-const VisKandidatCv = ({ cv }) => {
-    const utdanning = cv.utdanning.slice();
-    const yrkeserfaring = cv.yrkeserfaring.slice();
-    const kurs = cv.kurs.slice();
-    const sertifikater = cv.sertifikater.slice();
-    const sprak = cv.sprak.slice();
-
-    return (
-        <div className="panel--cv">
-            <Ekspanderbartpanel
-                id="ekspanderbartpanel-cv"
-                tittel="CV"
-                tittelProps="systemtittel"
-                apen
-            >
-                {cv.beskrivelse && (
-                    <Row className="panel--cv__row">
-                        <Column xs="12">
-                            <Undertittel>Sammendrag</Undertittel>
-                            <Normaltekst>{cv.beskrivelse}</Normaltekst>
-                        </Column>
-                    </Row>
-                )}
-                {utdanning && utdanning.length !== 0 && (
-                    <Row className="panel--cv__row">
-                        <Column xs="5">
-                            <Undertittel>Utdanning</Undertittel>
-                        </Column>
-                        <Column xs="7">
-                            {sortByDato(utdanning)
-                                .map((u) => (
-                                    <Row className="row--kategori" key={JSON.stringify(u)}>
-                                        <Undertekst className="cv--tidsperiode">
-                                            <Tidsperiode
-                                                fradato={u.fraDato}
-                                                tildato={u.tilDato}
-                                            />
-                                        </Undertekst>
-                                        <Element>{u.nusKodeUtdanningsnavn}</Element>
-                                        {u.utdannelsessted && (
-                                            <Normaltekst>{u.utdannelsessted}</Normaltekst>
-                                        )}
-                                        {u.alternativtUtdanningsnavn && (
-                                            <Normaltekst>{u.alternativtUtdanningsnavn}</Normaltekst>
-                                        )}
-                                    </Row>
-                                ))}
-                        </Column>
-                    </Row>
-                )}
-                {yrkeserfaring && yrkeserfaring.length !== 0 && (
-                    <Row className="panel--cv__row">
-                        <Column xs="5">
-                            <Undertittel>Arbeidserfaring</Undertittel>
-                        </Column>
-                        <Column xs="7">
-                            {sortByDato(yrkeserfaring)
-                                .map((a) => (
-                                    <Row className="row--kategori" key={JSON.stringify(a)}>
-                                        <Undertekst className="cv--tidsperiode">
-                                            <Tidsperiode
-                                                fradato={a.fraDato}
-                                                tildato={a.tilDato}
-                                            />
-                                        </Undertekst>
-                                        <Element>{a.styrkKodeStillingstittel}</Element>
-                                        {a.arbeidsgiver && (
-                                            <Normaltekst>{a.arbeidsgiver}</Normaltekst>
-                                        )}
-                                        {a.alternativStillingstittel && (
-                                            <Normaltekst>{a.alternativStillingstittel}</Normaltekst>
-                                        )}
-                                    </Row>
-                                ))}
-                        </Column>
-                    </Row>
-                )}
-                {kurs && kurs.length !== 0 && (
-                    <Row className="panel--cv__row">
-                        <Column xs="5">
-                            <Undertittel>Kurs og sertifiseringer</Undertittel>
-                        </Column>
-                        <Column xs="7">
-                            {sortByDato(kurs)
-                                .map((k) => (
-                                    <Row className="row--kategori" key={JSON.stringify(k)}>
-                                        <Undertekst className="cv--tidsperiode">
-                                            <Tidsperiode
-                                                fradato={k.fraDato}
-                                                tildato={k.tilDato}
-                                            />
-                                        </Undertekst>
-                                        {k.tittel && (
-                                            <Element>{k.tittel}</Element>
-                                        )}
-                                        {k.arrangor && (
-                                            <Normaltekst>{k.arrangor}</Normaltekst>
-                                        )}
-                                    </Row>
-                                ))}
-                        </Column>
-                    </Row>
-                )}
-                {sertifikater && sertifikater.length !== 0 && (
-                    <Row className="panel--cv__row">
-                        <Column xs="5">
-                            <Undertittel>Sertifikat</Undertittel>
-                        </Column>
-                        <Column xs="7">
-                            {sortByDato(sertifikater)
-                                .map((s) => (
-                                    <Row className="row--kategori" key={JSON.stringify(s)}>
-                                        <Undertekst className="cv--tidsperiode">
-                                            <Tidsperiode
-                                                fradato={s.fraDato}
-                                                tildato={s.tilDato}
-                                            />
-                                        </Undertekst>
-                                        {s.sertifikatKodeNavn && (
-                                            <Element>{s.sertifikatKodeNavn}</Element>
-                                        )}
-                                        {s.utsteder && (
-                                            <Normaltekst>{s.utsteder}</Normaltekst>
-                                        )}
-                                        {s.alternativtNavn && (
-                                            <Normaltekst>{s.alternativtNavn}</Normaltekst>
-                                        )}
-                                    </Row>
-                                ))}
-                        </Column>
-                    </Row>
-                )}
-                {sprak && sprak.length !== 0 && (
-                    <Row className="panel--cv__row">
-                        <Column xs="5">
-                            <Undertittel>Språk</Undertittel>
-                        </Column>
-                        <Column xs="7">
-                            {sprak.map((s) => (
-                                <Row className="row--kategori" key={JSON.stringify(s)}>
-                                    {s.kompetanseKodeTekst && (
-                                        <Element>{s.kompetanseKodeTekst}</Element>
+const VisKandidatCv = ({ cv }) => (
+    <div className="panel--cv">
+        <Ekspanderbartpanel
+            id="ekspanderbartpanel-cv"
+            tittel="CV"
+            tittelProps="systemtittel"
+            apen
+        >
+            {cv.beskrivelse && (
+                <Row className="panel--cv__row">
+                    <Column xs="12">
+                        <Undertittel>Sammendrag</Undertittel>
+                        <Normaltekst>{cv.beskrivelse}</Normaltekst>
+                    </Column>
+                </Row>
+            )}
+            {cv.utdanning && cv.utdanning.length !== 0 && (
+                <Row className="panel--cv__row">
+                    <Column xs="5">
+                        <Undertittel>Utdanning</Undertittel>
+                    </Column>
+                    <Column xs="7">
+                        {sortByDato(cv.utdanning)
+                            .map((u) => (
+                                <Row className="row--kategori" key={JSON.stringify(u)}>
+                                    <Undertekst className="cv--tidsperiode">
+                                        <Tidsperiode
+                                            fradato={u.fraDato}
+                                            tildato={u.tilDato}
+                                        />
+                                    </Undertekst>
+                                    <Element>{u.nusKodeUtdanningsnavn}</Element>
+                                    {u.utdannelsessted && (
+                                        <Normaltekst>{u.utdannelsessted}</Normaltekst>
                                     )}
-                                    {s.beskrivelse && (
-                                        <Normaltekst>{s.beskrivelse}</Normaltekst>
-                                    )}
-                                    {s.alternativTekst && (
-                                        <Normaltekst>{s.alternativTekst}</Normaltekst>
+                                    {u.alternativtUtdanningsnavn && (
+                                        <Normaltekst>{u.alternativtUtdanningsnavn}</Normaltekst>
                                     )}
                                 </Row>
                             ))}
-                        </Column>
-                    </Row>
-                )}
-            </Ekspanderbartpanel>
-        </div>
-    );
-};
+                    </Column>
+                </Row>
+            )}
+            {cv.yrkeserfaring && cv.yrkeserfaring.length !== 0 && (
+                <Row className="panel--cv__row">
+                    <Column xs="5">
+                        <Undertittel>Arbeidserfaring</Undertittel>
+                    </Column>
+                    <Column xs="7">
+                        {sortByDato(cv.yrkeserfaring)
+                            .map((a) => (
+                                <Row className="row--kategori" key={JSON.stringify(a)}>
+                                    <Undertekst className="cv--tidsperiode">
+                                        <Tidsperiode
+                                            fradato={a.fraDato}
+                                            tildato={a.tilDato}
+                                        />
+                                    </Undertekst>
+                                    <Element>{a.styrkKodeStillingstittel}</Element>
+                                    {a.arbeidsgiver && (
+                                        <Normaltekst>{a.arbeidsgiver}</Normaltekst>
+                                    )}
+                                    {a.alternativStillingstittel && (
+                                        <Normaltekst>{a.alternativStillingstittel}</Normaltekst>
+                                    )}
+                                </Row>
+                            ))}
+                    </Column>
+                </Row>
+            )}
+            {cv.kurs && cv.kurs.length !== 0 && (
+                <Row className="panel--cv__row">
+                    <Column xs="5">
+                        <Undertittel>Kurs og sertifiseringer</Undertittel>
+                    </Column>
+                    <Column xs="7">
+                        {sortByDato(cv.kurs)
+                            .map((k) => (
+                                <Row className="row--kategori" key={JSON.stringify(k)}>
+                                    <Undertekst className="cv--tidsperiode">
+                                        <Tidsperiode
+                                            fradato={k.fraDato}
+                                            tildato={k.tilDato}
+                                        />
+                                    </Undertekst>
+                                    {k.tittel && (
+                                        <Element>{k.tittel}</Element>
+                                    )}
+                                    {k.arrangor && (
+                                        <Normaltekst>{k.arrangor}</Normaltekst>
+                                    )}
+                                </Row>
+                            ))}
+                    </Column>
+                </Row>
+            )}
+            {cv.sertifikater && cv.sertifikater.length !== 0 && (
+                <Row className="panel--cv__row">
+                    <Column xs="5">
+                        <Undertittel>Sertifikat</Undertittel>
+                    </Column>
+                    <Column xs="7">
+                        {sortByDato(cv.sertifikater)
+                            .map((s) => (
+                                <Row className="row--kategori" key={JSON.stringify(s)}>
+                                    <Undertekst className="cv--tidsperiode">
+                                        <Tidsperiode
+                                            fradato={s.fraDato}
+                                            tildato={s.tilDato}
+                                        />
+                                    </Undertekst>
+                                    {s.sertifikatKodeNavn && (
+                                        <Element>{s.sertifikatKodeNavn}</Element>
+                                    )}
+                                    {s.utsteder && (
+                                        <Normaltekst>{s.utsteder}</Normaltekst>
+                                    )}
+                                    {s.alternativtNavn && (
+                                        <Normaltekst>{s.alternativtNavn}</Normaltekst>
+                                    )}
+                                </Row>
+                            ))}
+                    </Column>
+                </Row>
+            )}
+            {cv.sprak && cv.sprak.length !== 0 && (
+                <Row className="panel--cv__row">
+                    <Column xs="5">
+                        <Undertittel>Språk</Undertittel>
+                    </Column>
+                    <Column xs="7">
+                        {cv.sprak.map((s) => (
+                            <Row className="row--kategori" key={JSON.stringify(s)}>
+                                {s.kompetanseKodeTekst && (
+                                    <Element>{s.kompetanseKodeTekst}</Element>
+                                )}
+                                {s.beskrivelse && (
+                                    <Normaltekst>{s.beskrivelse}</Normaltekst>
+                                )}
+                                {s.alternativTekst && (
+                                    <Normaltekst>{s.alternativTekst}</Normaltekst>
+                                )}
+                            </Row>
+                        ))}
+                    </Column>
+                </Row>
+            )}
+        </Ekspanderbartpanel>
+    </div>
+);
 
 
 VisKandidatCv.propTypes = {
