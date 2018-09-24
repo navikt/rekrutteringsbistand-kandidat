7e80c05bcf7a724db4e908b5e44fd593465337d8
Eirik Vilpponen <eirik.vilpponen@nav.no>
2018-09-24 15:09:36 +0200

    PAM-1263 Styling på header. Legge til Ikoner.

---
+++ src/common/ikoner/AdresseIkon.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import * as React from 'react';
+const AdresseIkon = () =>
+    (
+        <svg width="30" height="30" viewBox="0 0 24 24">
+            <g stroke="#000" fill="none">
+                <path d="M19.5 8c0 4.144-7.5 15.5-7.5 15.5s-7.5-11.356-7.5-15.5c0-4.142 3.357-7.5 7.5-7.5 4.142 0 7.5 3.358 7.5 7.5z" fill="#FFFFFF"/>
+                <circle cx="12" cy="8" r="3"/>
+            </g>
+        </svg>
+    );
+export default AdresseIkon;
--- src/result/visKandidat/VisKandidatPersonalia.js
+++ src/result/visKandidat/VisKandidatPersonalia.js
@@ -1,4 +1,4 @@
-import { Column, Row, Container } from 'nav-frontend-grid';
+import { Row } from 'nav-frontend-grid';
 import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
 import React from 'react';
 import connect from 'react-redux/es/connect/connect';
@@ -6,6 +6,9 @@ import { Link } from 'react-router-dom';
 import NavFrontendChevron from 'nav-frontend-chevron';
 import { formatISOString } from '../../common/dateUtils';
 import cvPropTypes from '../../PropTypes';
+import TelefonIkon from '../../common/ikoner/TelefonIkon';
+import MailIkon from '../../common/ikoner/MailIkon';
+import AdresseIkon from '../../common/ikoner/AdresseIkon';
 
 class VisKandidatPersonalia extends React.Component {
     render() {
@@ -28,35 +31,40 @@ class VisKandidatPersonalia extends React.Component {
                         <Normaltekst className="header--personalia__fodselsdato">Fødselsdato: {formatISOString(cv.fodselsdato, 'D. MMMM YYYY')}</Normaltekst>
                     )}
                 </Row>
-
-                <Container>
-
-                    <Column sm="4" >
-                        {(cv.epost) && (
-                            <Normaltekst>
-                                <a href={`mailto:${cv.epost}`} className="lenke header--personalia__mail">{cv.epost}</a>
+                <div className="personalia-container" >
+                    {(cv.epost) && (
+                        <div className="personalia--item">
+                            <MailIkon />
+                            <Normaltekst className="header--personalia">
+                                <a href={`mailto:${cv.epost}`} className="header--personalia__mail">{cv.epost}</a>
                             </Normaltekst>
-                        )}
-                    </Column>
-                    <Column sm="4" className="header--personalia">
-                        {cv.telefon && (
-                            <Normaltekst>
-                                <strong>Telefon: {cv.telefon}</strong>
+
+                        </div>
+                    )}
+                    {cv.telefon && (
+                        <div className="personalia--item">
+                            <TelefonIkon />
+                            <Normaltekst className="header--personalia">
+                                <strong>{cv.telefon}</strong>
                             </Normaltekst>
-                        )}
-                    </Column>
-                    <Column sm="4" >
-                        {cv.adresse && cv.adresse.adrlinje1 && (
+
+                        </div>
+                    )}
+                    {cv.adresse && cv.adresse.adrlinje1 && (
+                        <div className="personalia--item">
+
+                            <AdresseIkon />
                             <Normaltekst className="header--personalia">
                                 {cv.adresse.adrlinje1}
                                 {(cv.adresse.postnr || cv.adresse.poststednavn) ?
                                     (', ') : null}
                                 {cv.adresse.postnr} {cv.adresse.poststednavn}
                             </Normaltekst>
-                        )}
-                    </Column>
 
-                </Container>
+                        </div>
+                    )}
+
+                </div>
 
             </div>
         );
