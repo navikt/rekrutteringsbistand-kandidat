734e4dec229125b16e09daf5b92ac8954c601ece
Kristian Storvoll <kristian storvoll>
2018-09-24 15:39:27 +0200

    PAM-1458: Endrer tittel i kandidatsøk til 'Kandidatsøk'

--- src/result/ResultatVisning.js
+++ src/result/ResultatVisning.js
@@ -50,7 +50,7 @@ class ResultatVisning extends React.Component {
                         <Container className="blokk-s container--wide">
                             <Row>
                                 <Column className="text-center">
-                                    <Sidetittel>Aktuelle kandidater</Sidetittel>
+                                    <Sidetittel>Kandidatsøk</Sidetittel>
                                 </Column>
                             </Row>
                             <Row className="resultatvisning--body">
