65fba0c6d1e3080fde573c9d81bbf5d5ac25837e
Eirik Vilpponen <eirik.vilpponen@nav.no>
2018-09-25 15:40:24 +0200

    PAM-1263 Nytt adresse-ikon. Setter stor forbokstav på fornavn/etternavn. Styling på VisKandidatPersonalia

--- src/common/ikoner/AdresseIkon.js
+++ src/common/ikoner/AdresseIkon.js
@@ -2,10 +2,14 @@
 import * as React from 'react';
 const AdresseIkon = () =>
     (
-        <svg width="30" height="30" viewBox="0 0 24 24">
-            <g stroke="#000" fill="none">
-                <path d="M19.5 8c0 4.144-7.5 15.5-7.5 15.5s-7.5-11.356-7.5-15.5c0-4.142 3.357-7.5 7.5-7.5 4.142 0 7.5 3.358 7.5 7.5z" fill="#FFFFFF"/>
-                <circle cx="12" cy="8" r="3"/>
+        <svg width="23px" height="34px" viewBox="0 0 23 34" version="1.1"  >
+            <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+                <g id="header/-profil" transform="translate(-868.000000, -168.000000)" stroke="#FFFFFF">
+                    <g id="location-pin-3" transform="translate(868.000000, 168.000000)">
+                        <path d="M21.9583333,11.3333333 C21.9583333,17.204 11.3333333,33.2916667 11.3333333,33.2916667 C11.3333333,33.2916667 0.708333333,17.204 0.708333333,11.3333333 C0.708333333,5.4655 5.46408333,0.708333333 11.3333333,0.708333333 C17.2011667,0.708333333 21.9583333,5.4655 21.9583333,11.3333333 Z" id="Shape"/>
+                        <circle id="Oval" cx="11.3333333" cy="11.3333333" r="4.25"/>
+                    </g>
+                </g>
             </g>
         </svg>
     );
--- src/result/visKandidat/VisKandidatPersonalia.js
+++ src/result/visKandidat/VisKandidatPersonalia.js
@@ -1,4 +1,4 @@
-import { Row } from 'nav-frontend-grid';
+import { Column, Row } from 'nav-frontend-grid';
 import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
 import React from 'react';
 import connect from 'react-redux/es/connect/connect';
@@ -11,62 +11,83 @@ import MailIkon from '../../common/ikoner/MailIkon';
 import AdresseIkon from '../../common/ikoner/AdresseIkon';
 
 class VisKandidatPersonalia extends React.Component {
+    capitalizeFirstLetter = (inputString) => inputString.charAt(0).toUpperCase() + inputString.slice(1);
+
     render() {
         const cv = this.props.cv;
+        cv.telefon = '12312312';
+
+        let fornavnStorForbokstav;
+        if (cv.fornavn) {
+            fornavnStorForbokstav = this.capitalizeFirstLetter(cv.fornavn.toLowerCase());
+        }
+        let etternavnStorForbokstav;
+        if (cv.etternavn) {
+            etternavnStorForbokstav = this.capitalizeFirstLetter(cv.etternavn.toLowerCase());
+        }
+
         return (
             <div className="header--bakgrunn">
-                <Link
-                    to={'/pam-kandidatsok'}
-                    className="header--personalia__lenke"
-                >
-                    <NavFrontendChevron type="venstre" /> Til kandidatsøk
-                </Link>
 
-                <Row xs="12">
+                <Row>
+                    <Link
+                        to={'/pam-kandidatsok'}
+                        className="header--personalia__lenke"
+                    >
+                        <NavFrontendChevron type="venstre" /> Til kandidatsøk
+                    </Link>
+                </Row>
 
-                    <Sidetittel className="header--personalia">
-                        {cv.fornavn} {cv.etternavn}
+                <Row>
+                    <Sidetittel className="header--personalia__overskrift">
+                        {fornavnStorForbokstav} {etternavnStorForbokstav}
                     </Sidetittel>
                     {cv.fodselsdato && (
                         <Normaltekst className="header--personalia__fodselsdato">Fødselsdato: {formatISOString(cv.fodselsdato, 'D. MMMM YYYY')}</Normaltekst>
                     )}
                 </Row>
-                <div className="personalia-container" >
-                    {(cv.epost) && (
-                        <div className="personalia--item">
-                            <MailIkon />
-                            <Normaltekst className="header--personalia">
-                                <a href={`mailto:${cv.epost}`} className="header--personalia__mail">{cv.epost}</a>
-                            </Normaltekst>
+                <Row>
+                    <div className="personalia-container">
+                        {(cv.epost) && (
+                            <div className="personalia--item">
+                                <div>
+                                    <MailIkon />
+                                </div>
+                                <Normaltekst className="header--personalia__tekst">
+                                    <a href={`mailto:${cv.epost}`} className="header--personalia__mail">{cv.epost}</a>
+                                </Normaltekst>
 
-                        </div>
-                    )}
-                    {cv.telefon && (
-                        <div className="personalia--item">
-                            <TelefonIkon />
-                            <Normaltekst className="header--personalia">
-                                <strong>{cv.telefon}</strong>
-                            </Normaltekst>
+                            </div>
+                        )}
+                        {cv.telefon && (
+                            <div className="personalia--item">
+                                <div>
+                                    <TelefonIkon />
+                                </div>
 
-                        </div>
-                    )}
-                    {cv.adresse && cv.adresse.adrlinje1 && (
-                        <div className="personalia--item">
+                                <Normaltekst className="header--personalia__tekst">
+                                    <strong>{cv.telefon}</strong>
+                                </Normaltekst>
 
-                            <AdresseIkon />
-                            <Normaltekst className="header--personalia">
+                            </div>
+                        )}
+                        {cv.adresse && cv.adresse.adrlinje1 && <div className="personalia--item">
+
+                            <div>
+                                <AdresseIkon />
+                            </div>
+                            <Normaltekst className="header--personalia__tekst">
                                 {cv.adresse.adrlinje1}
                                 {(cv.adresse.postnr || cv.adresse.poststednavn) ?
                                     (', ') : null}
+
                                 {cv.adresse.postnr} {cv.adresse.poststednavn}
                             </Normaltekst>
-
-                        </div>
-                    )}
-
-                </div>
-
+                        </div>}
+                    </div>
+                </Row>
             </div>
+
         );
     }
 }
