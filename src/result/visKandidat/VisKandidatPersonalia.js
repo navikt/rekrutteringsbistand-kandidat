635309d262aa1d595ec13f47eb1e9ddd95a45c22
Eirik Vilpponen <eirik.vilpponen@nav.no>
2018-09-26 09:17:29 +0200

    PAM-1263 Manuell merge av VisKandidatPersonalia. Legger til arbeid fra siste uke.

--- src/result/visKandidat/VisKandidatPersonalia.js
+++ src/result/visKandidat/VisKandidatPersonalia.js
@@ -1,59 +1,92 @@
-import { Column, Row } from 'nav-frontend-grid';
+import { Row } from 'nav-frontend-grid';
 import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
 import React from 'react';
 import connect from 'react-redux/es/connect/connect';
 import { Link } from 'react-router-dom';
+import NavFrontendChevron from 'nav-frontend-chevron';
 import { formatISOString } from '../../common/dateUtils';
 import cvPropTypes from '../../PropTypes';
+import TelefonIkon from '../../common/ikoner/TelefonIkon';
+import MailIkon from '../../common/ikoner/MailIkon';
+import AdresseIkon from '../../common/ikoner/AdresseIkon';
 
 class VisKandidatPersonalia extends React.Component {
+    capitalizeFirstLetter = (inputString) => inputString.charAt(0).toUpperCase() + inputString.slice(1);
+
     render() {
         const cv = this.props.cv;
+
+        let fornavnStorForbokstav;
+        if (cv.fornavn) {
+            fornavnStorForbokstav = this.capitalizeFirstLetter(cv.fornavn.toLowerCase());
+        }
+        let etternavnStorForbokstav;
+        if (cv.etternavn) {
+            etternavnStorForbokstav = this.capitalizeFirstLetter(cv.etternavn.toLowerCase());
+        }
+
         return (
-            <div>
-                <Row className="blokk-s personalia--modal">
-                    <Column xs="12">
-                        <Link
-                            to={'/pam-kandidatsok'}
-                        >
-                            Tilbake til sok
-                        </Link>
-                        <Sidetittel className="navn--modal">
-                            {cv.fornavn} {cv.etternavn}
-                        </Sidetittel>
-                        {cv.fodselsdato && (
-                            <Normaltekst><strong>Fødselsdato:</strong> {formatISOString(cv.fodselsdato, 'D. MMMM YYYY')}</Normaltekst>
-                        )}
-                        {cv.adresse && cv.adresse.adrlinje1 && (
-                            <Normaltekst>
-                                <strong>Adresse:</strong> {cv.adresse.adrlinje1}
-                                {(cv.adresse.postnr || cv.adresse.poststednavn) ?
-                                    (', ') : null}
-                                {cv.adresse.postnr} {cv.adresse.poststednavn}
-                            </Normaltekst>
-                        )}
-                        {(cv.epost || cv.telefon) && (
-                            <div className="kontakt--modal">
-                                {cv.epost && (
-                                    <Normaltekst>
-                                        <i className="mail--icon" />
-                                        <strong>E-post:</strong>
-                                        <a href={`mailto:${cv.epost}`} className="lenke mail--text">{cv.epost}</a>
-                                    </Normaltekst>
-                                )}
-                                {cv.telefon && (
-                                    <Normaltekst>
-                                        <i className="telefon--icon" />
-                                        <strong>Telefon:</strong> {/* TODO: Telefon er ikke med
-                                    fra backend per nå, oppdatere denne når det er med */}
-                                        {/* <a href={`tel:${cv.telefon}`} className="lenke telefon--text">{cv.telefon}</a> */}
-                                    </Normaltekst>
-                                )}
+            <div className="header--bakgrunn">
+
+                <Row>
+                    <Link
+                        to={'/pam-kandidatsok'}
+                        className="header--personalia__lenke"
+                    >
+                        <NavFrontendChevron type="venstre" /> Til kandidatsøk
+                    </Link>
+                </Row>
+
+                <Row>
+                    <Sidetittel className="header--personalia__overskrift">
+                        {fornavnStorForbokstav} {etternavnStorForbokstav}
+                    </Sidetittel>
+                    {cv.fodselsdato && (
+                        <Normaltekst className="header--personalia__fodselsdato">Fødselsdato: {formatISOString(cv.fodselsdato, 'D. MMMM YYYY')}</Normaltekst>
+                    )}
+                </Row>
+                <Row>
+                    <div className="personalia-container">
+                        {(cv.epost) && (
+                            <div className="personalia--item">
+                                <div>
+                                    <MailIkon />
+                                </div>
+                                <Normaltekst className="header--personalia__tekst">
+                                    <a href={`mailto:${cv.epost}`} className="header--personalia__mail">{cv.epost}</a>
+                                </Normaltekst>
+
                             </div>
                         )}
-                    </Column>
+                        {cv.telefon && (
+                            <div className="personalia--item">
+                                <div>
+                                    <TelefonIkon />
+                                </div>
+
+                                <Normaltekst className="header--personalia__tekst">
+                                    <strong>{cv.telefon}</strong>
+                                </Normaltekst>
+
+                            </div>
+                        )}
+                        {cv.adresse && cv.adresse.adrlinje1 && <div className="personalia--item">
+
+                            <div>
+                                <AdresseIkon />
+                            </div>
+                            <Normaltekst className="header--personalia__tekst">
+                                {cv.adresse.adrlinje1}
+                                {(cv.adresse.postnr || cv.adresse.poststednavn) ?
+                                    (', ') : null}
+
+                                {cv.adresse.postnr} {cv.adresse.poststednavn}
+                            </Normaltekst>
+                        </div>}
+                    </div>
                 </Row>
             </div>
+
         );
     }
 }
