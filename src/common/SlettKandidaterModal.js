b022ac34aff1712c9939d447db9d3f9316b5ff50
Marthe Skogen <martheskogen@bekk.no>
2018-10-10 10:13:05 +0200

    Trekker ut SlettKandidaterModal i egen komponent for gjenbruk

---
+++ src/common/SlettKandidaterModal.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'nav-frontend-modal';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
+import { Flatknapp, Hovedknapp } from 'nav-frontend-knapper';
+import { fornavnOgEtternavnFraKandidat } from '../sok/utils';
+import cvPropTypes from '../PropTypes';
+
+const SlettKandidaterModal = ({ isOpen, sletterKandidater, lukkModal, valgteKandidater, visFeilmelding, onDeleteClick }) => (
+    <Modal
+        className="KandidatlisteDetalj__modal"
+        isOpen={isOpen}
+        onRequestClose={() => {
+            if (!sletterKandidater) {
+                lukkModal();
+            }
+        }}
+        closeButton
+        contentLabel={valgteKandidater.length === 1 ? 'Slett kandidat' : 'Slett kandidatene'}
+    >
+        {visFeilmelding && (
+            <AlertStripeAdvarsel className="feilmleding">Noe gikk galt under sletting av
+                kandidater</AlertStripeAdvarsel>
+        )}
+        <Sidetittel
+            className="overskrift"
+        >{valgteKandidater.length === 1 ? 'Slett kandidat' : 'Slett kandidatene'}</Sidetittel>
+        <Normaltekst>{valgteKandidater.length === 1
+            ? `Er du sikker på at du ønsker å slette ${fornavnOgEtternavnFraKandidat(valgteKandidater.pop())} fra listen?`
+            : 'Er du sikker på at du ønsker å slette kandidatene fra listen?'
+        }
+        </Normaltekst>
+        <div className="knapperad">
+            <Hovedknapp onClick={onDeleteClick}>Slett</Hovedknapp>
+            <Flatknapp onClick={lukkModal} disabled={sletterKandidater}>Avbryt</Flatknapp>
+        </div>
+    </Modal>
+);
+
+SlettKandidaterModal.defaultProps = {
+    valgteKandidater: []
+};
+
+SlettKandidaterModal.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    sletterKandidater: PropTypes.bool.isRequired,
+    lukkModal: PropTypes.func.isRequired,
+    valgteKandidater: PropTypes.oneOf(PropTypes.arrayOf(
+        PropTypes.shape({
+            lagtTilAv: PropTypes.string,
+            kandidatnr: PropTypes.string,
+            sisteArbeidserfaring: PropTypes.string,
+            fornavn: PropTypes.string,
+            etternavn: PropTypes.string
+        })), PropTypes.arrayOf(cvPropTypes.isRequired)
+    ),
+    visFeilmelding: PropTypes.bool.isRequired,
+    onDeleteClick: PropTypes.func.isRequired
+};
+
+export default SlettKandidaterModal;
