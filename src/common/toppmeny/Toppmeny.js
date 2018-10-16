385002b80f1db2c7cd6badc36369b1a8c759173b
Aksel Wester <aksel@wester.co>
2018-10-16 16:45:26 +0200

    PAM-1441 Legger til prop-types og fikser formattering av arbeidsgiverselect

--- src/common/toppmeny/Toppmeny.js
+++ src/common/toppmeny/Toppmeny.js
@@ -1,6 +1,5 @@
-/* eslint-disable react/prop-types */
-
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { LOGOUT_URL } from '../fasitProperties';
 import HeaderMeny, { TAB_ID } from '../module/HeaderMeny';
@@ -10,26 +9,40 @@ const loggUt = () => {
     window.location.href = LOGOUT_URL;
 };
 
-const toppmenyForTab = (activeTabID) => (
-    ({ arbeidsgivere, valgtArbeidsgiverId, velgArbeidsgiver, resetArbeidsgiver }) => {
-        const onArbeidsgiverSelect = (orgNummer) => {
-            if (orgNummer) {
-                velgArbeidsgiver(orgNummer);
-            } else {
-                resetArbeidsgiver();
-            }
-        };
-        return (
-            <HeaderMeny
-                onLoggUt={loggUt}
-                onArbeidsgiverSelect={onArbeidsgiverSelect}
-                arbeidsgivere={arbeidsgivere}
-                valgtArbeidsgiverId={valgtArbeidsgiverId}
-                activeTabID={activeTabID}
-            />
-        );
-    }
-);
+const Toppmeny = ({ arbeidsgivere, valgtArbeidsgiverId, velgArbeidsgiver, resetArbeidsgiver, activeTabID }) => {
+    const onArbeidsgiverSelect = (orgNummer) => {
+        if (orgNummer) {
+            velgArbeidsgiver(orgNummer);
+        } else {
+            resetArbeidsgiver();
+        }
+    };
+    return (
+        <HeaderMeny
+            onLoggUt={loggUt}
+            onArbeidsgiverSelect={onArbeidsgiverSelect}
+            arbeidsgivere={arbeidsgivere}
+            valgtArbeidsgiverId={valgtArbeidsgiverId}
+            activeTabID={activeTabID}
+        />
+    );
+};
+
+Toppmeny.defaultProps = {
+    valgtArbeidsgiverId: undefined
+};
+
+Toppmeny.propTypes = {
+    arbeidsgivere: PropTypes.arrayOf(PropTypes.shape({
+        orgnr: PropTypes.string,
+        orgnavn: PropTypes.string
+    })).isRequired,
+    valgtArbeidsgiverId: PropTypes.string,
+    velgArbeidsgiver: PropTypes.func.isRequired,
+    resetArbeidsgiver: PropTypes.func.isRequired,
+    activeTabID: PropTypes.string.isRequired
+};
+
 
 const mapStateToProps = (state) => ({
     arbeidsgivere: state.mineArbeidsgivere.arbeidsgivere,
@@ -41,6 +54,14 @@ const mapDispatchToProps = (dispatch) => ({
     resetArbeidsgiver: () => dispatch({ type: RESET_ARBEIDSGIVER })
 });
 
-export const KandidatsokHeader = connect(mapStateToProps, mapDispatchToProps)(toppmenyForTab(TAB_ID.KANDIDATSOK));
-export const KandidatlisteHeader = connect(mapStateToProps, mapDispatchToProps)(toppmenyForTab(TAB_ID.KANDIDATLISTER));
+const KandidatsokHeaderComponent = (props) => (
+    <Toppmeny {...props} activeTabID={TAB_ID.KANDIDATSOK} />
+);
 
+const KandidatlisteHeaderComponent = (props) => (
+    <Toppmeny {...props} activeTabID={TAB_ID.KANDIDATLISTER} />
+);
+
+export const KandidatsokHeader = connect(mapStateToProps, mapDispatchToProps)(KandidatsokHeaderComponent);
+
+export const KandidatlisteHeader = connect(mapStateToProps, mapDispatchToProps)(KandidatlisteHeaderComponent);
