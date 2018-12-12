f170b0ff03b31a549b9fec896369485d77dd4c45
Johansen, Erlend <Erlend.Johansen@nav.no>
2018-12-12 12:28:40 +0100

    PAM-2223 Lager egen funksjon for om ordet skal være i små eller store bokstaver, etter tilbakemelding fra review. app:veil

--- src/felles/sok/utils.js
+++ src/felles/sok/utils.js
@@ -188,35 +188,40 @@ export const formatterStedsnavn = (inputString) => inputString
     .join(' ');
 
 
-export const capitalizeEmployerName = (text) => {
+export const ordToCorrectCase = (ord, listeMedUpperCaseOrd, listeMedLowerCaseOrd) => {
+    if (listeMedUpperCaseOrd.includes(ord)) {
+        return ord.toUpperCase();
+    } else if (!listeMedLowerCaseOrd.includes(ord)) {
+        if (ord[0] !== undefined) {
+            return ord[0].toUpperCase() + ord.substr(1);
+        }
+    }
+    return ord;
+};
+
+export const capitalizeEmployerName = (employerName) => {
     const separators = [' ', '-', '(', '/'];
 
-    const ignore = [
+    const lowerCaseOrd = [
         'i', 'og', 'for', 'på', 'avd', 'av'
     ];
 
-    const keep = [
+    const upperCaseOrd = [
         'as', 'ab', 'asa', 'ba', 'sa'
     ];
 
-    if (text) {
-        let capitalized = text.toLowerCase();
+    if (employerName) {
+        let text = employerName.toLowerCase();
 
-        for (let i = 0, len = separators.length; i < len; i += 1) {
-            const fragments = capitalized.split(separators[i]);
-            for (let j = 0, x = fragments.length; j < x; j += 1) {
-                if (keep.includes(fragments[j])) {
-                    fragments[j] = fragments[j].toUpperCase();
-                } else if (!ignore.includes(fragments[j])) {
-                    if (fragments[j][0] !== undefined) {
-                        fragments[j] = fragments[j][0].toUpperCase() + fragments[j].substr(1);
-                    }
-                }
+        for (let i = 0; i < separators.length; i += 1) {
+            const fragments = text.split(separators[i]);
+            for (let j = 0; j < fragments.length; j += 1) {
+                fragments[j] = ordToCorrectCase(fragments[j], upperCaseOrd, lowerCaseOrd);
             }
-            capitalized = fragments.join(separators[i]);
+            text = fragments.join(separators[i]);
         }
 
-        return capitalized;
+        return text;
     }
-    return text;
+    return employerName;
 };
