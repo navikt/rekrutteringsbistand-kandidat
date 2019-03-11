909a1b91a5b1e5d869d4ffb0529efaa8168e0847
Tony Gjendahl <tony.gjendahl@nav.no>
2019-03-11 12:47:54 +0100

    PAM-2802 Catche searchapi-feilen slik at siden ikke trenger refresh om man ikke finner bedrift, app:veil

--- src/veileder/common/typeahead/enhetsregisterReducer.js
+++ src/veileder/common/typeahead/enhetsregisterReducer.js
@@ -1,5 +1,5 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { fetchArbeidsgivereEnhetsregister, fetchArbeidsgivereEnhetsregisterOrgnr } from '../../api';
+import { fetchArbeidsgivereEnhetsregister, fetchArbeidsgivereEnhetsregisterOrgnr, SearchApiError } from '../../api';
 
 /** *********************************************************
  * ACTIONS
@@ -68,7 +68,11 @@ function* fetchTypeAheadSuggestions(action) {
                 query: value
             });
         } catch (e) {
-            throw e;
+            if (e instanceof SearchApiError) {
+                // Do nothing for now
+            } else {
+                throw e;
+            }
         }
     }
 }
