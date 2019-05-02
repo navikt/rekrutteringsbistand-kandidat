366301ff1e46934c5cdaa0298400c1ca4f085938
Tony Gjendahl <tony.gjendahl@nav.no>
2019-05-02 11:05:55 +0200

    Fix - Oppdatere hover på typeahead, app:arb,veil

--- src/veileder/common/typeahead/Typeahead.js
+++ src/veileder/common/typeahead/Typeahead.js
@@ -185,7 +185,7 @@ export default class Typeahead extends React.Component {
                     ref={(input) => {
                         this.input = input;
                     }}
-                    className="skjemaelement__input input--fullbredde skjemaelement--pink"
+                    className="skjemaelement__input input--fullbredde skjemaelement__blue"
                 />
                 <Knapp
                     aria-label="søk"
