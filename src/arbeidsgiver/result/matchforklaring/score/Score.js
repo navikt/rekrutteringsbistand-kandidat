906f03941eea8fe863a892f749be70be59eaa8bc
Ida Onshus <Ida.Onshus@nav.no>
2019-05-07 11:45:16 +0200

    Legger på arialabel på match-score

--- src/arbeidsgiver/result/matchforklaring/score/Score.js
+++ src/arbeidsgiver/result/matchforklaring/score/Score.js
@@ -5,8 +5,21 @@ import './Score.less';
 
 export default function Score({ value, isTotalScore = false }) {
     const limitEnum = isTotalScore ? TotalScoreLimitEnum : ScoreLimitEnum;
+    let ariaLabel = 'Match 0 av 5';
+    if (value >= limitEnum.LIMIT_5) {
+        ariaLabel = 'Match 5 av 5';
+    } else if (value >= limitEnum.LIMIT_4) {
+        ariaLabel = 'Match 4 av 5';
+    } else if (value >= limitEnum.LIMIT_3) {
+        ariaLabel = 'Match 3 av 5';
+    } else if (value >= limitEnum.LIMIT_2) {
+        ariaLabel = 'Match 2 av 5';
+    } else if (value >= limitEnum.LIMIT_1) {
+        ariaLabel = 'Match 1 av 5';
+    }
+
     return (
-        <div className="match-score">
+        <div className="match-score" aria-label={ariaLabel}>
             <div className={value >= limitEnum.LIMIT_1 ? 'scoreIcon--filled' : 'scoreIcon'} />
             <div className={value >= limitEnum.LIMIT_2 ? 'scoreIcon--filled' : 'scoreIcon'} />
             <div className={value >= limitEnum.LIMIT_3 ? 'scoreIcon--filled' : 'scoreIcon'} />
