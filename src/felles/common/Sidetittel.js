68b20cbf659234fc9b18dd9d8037e42fe007ae99
Stephen Ramthun <stephen.ramthun@nav.no>
2019-05-09 08:25:51 +0200

    Fiks akseptansetester app:arb,veil

--- src/felles/common/Sidetittel.js
+++ src/felles/common/Sidetittel.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Sidetittel = ({ children, className }) => (
-    <h1 className={`h1 display-1 ${className}`} style={{ fontFamily: 'Source Sans Pro' }}>{children}</h1>
+const Sidetittel = ({ children, className, id }) => (
+    <h1
+        id={id}
+        className={`h1 display-1 ${className}`}
+        style={{ fontFamily: 'Source Sans Pro' }}
+    >
+        {children}
+    </h1>
 );
 
 Sidetittel.propTypes = {
     children: PropTypes.string.isRequired,
-    className: PropTypes.string
+    className: PropTypes.string,
+    id: PropTypes.string
 };
 
 Sidetittel.defaultProps = {
-    className: ''
+    className: '',
+    id: 'sidetittel'
 };
 
 export default Sidetittel;
