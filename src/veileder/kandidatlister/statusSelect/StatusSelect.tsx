ace626835e992f194d352f7ab5c086bee9631a2a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2019-11-28 13:04:33 +0100

    Fargesymboler på menu for status i kandidatlister app:veil

---
+++ src/veileder/kandidatlister/statusSelect/StatusSelect.tsx
@@ -0,0 +1,86 @@
+import React, { FunctionComponent } from 'react';
+import { Menu, MenuList, MenuButton, MenuItem } from '@reach/menu-button';
+import { Normaltekst } from 'nav-frontend-typografi';
+import '@reach/menu-button/styles.css';
+import './StatusSelect.less';
+
+enum Status {
+    Foreslått = 'FORESLATT',
+    Vurderes = 'VURDERES',
+    Kontaktet = 'KONTAKTET',
+    Aktuell = 'AKTUELL',
+    Uaktuell = 'UAKTUELL',
+    Uinteressert = 'UINTERESSERT',
+}
+
+const statusToDisplayName = (status: Status) => {
+    switch (status) {
+        case Status.Foreslått:
+            return 'Foreslått';
+        case Status.Vurderes:
+            return 'Vurderes';
+        case Status.Kontaktet:
+            return 'Kontaktet';
+        case Status.Aktuell:
+            return 'Aktuell';
+        case Status.Uaktuell:
+            return 'Ikke aktuell';
+        case Status.Uinteressert:
+            return 'Ikke interessert';
+    }
+};
+
+const statusToClassname = (status: Status) => status.toLowerCase();
+
+interface Props {
+    kanEditere: boolean;
+    value: Status;
+    onChange: (status: Status) => void;
+}
+
+const StatusSelect: FunctionComponent<Props> = ({ kanEditere, value, onChange }) => {
+    if (!kanEditere) {
+        return <Statusvisning status={value} />;
+    }
+
+    const statuserIDropdown = Object.keys(Status);
+
+    return (
+        <div className="StatusSelect skjemaelement skjemaelement--pink">
+            <Menu>
+                <MenuButton className="StatusSelect__button selectContainer skjemaelement__input">
+                    <Statusvisning status={value} />
+                </MenuButton>
+                <MenuList className="StatusSelect__menu">
+                    {statuserIDropdown.map(status => (
+                        <MenuItem
+                            key={status}
+                            onSelect={() => onChange(Status[status])}
+                            className="StatusSelect__menuItem"
+                            aria-selected={Status[status] === value}
+                        >
+                            <Statusvisning status={Status[status]} />
+                        </MenuItem>
+                    ))}
+                </MenuList>
+            </Menu>
+        </div>
+    );
+};
+
+interface StatusvisningProps {
+    status: Status;
+}
+
+const Statusvisning: FunctionComponent<StatusvisningProps> = ({ status }) => (
+    <span className="StatusSelect__status">
+        <span
+            className={`StatusSelect__status__sirkel StatusSelect__status__sirkel--${statusToClassname(
+                status
+            )}`}
+        />
+        <Normaltekst>{statusToDisplayName(status)}</Normaltekst>
+    </span>
+);
+
+export default StatusSelect;
