98a1896192229d5e88b69fc7541e186c2315da72
Julian Torp <julian.torp@nav.no>
2019-12-20 13:46:28 +0100

    add error message on altinn requests app:arb

--- src/arbeidsgiver/sok/error/Feilmelding.js
+++ src/arbeidsgiver/sok/error/Feilmelding.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { AlertStripeFeil } from 'nav-frontend-alertstriper';
+import { AlertStripeFeil, AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
 import { Hovedknapp } from 'pam-frontend-knapper';
 
 function refreshPage() {
@@ -10,13 +10,23 @@ function refreshPage() {
 const Feilmelding = (props) => {
     const { overskrift, melding } = props;
     return (
-        <AlertStripeFeil className="blokk-xs alertstripe--solid Feilmelding__alertstripe">
-            <div>
-                <strong>{overskrift}</strong><br />
-                {melding}
-            </div>
-            <Hovedknapp mini onClick={refreshPage}>Last siden på nytt</Hovedknapp>
-        </AlertStripeFeil>
+        <div>
+            <AlertStripeAdvarsel className="blokk-xs alertstripe--solid Feilmelding__alertstripe">
+                <div>
+                    <strong>Nedetid for arbeidsgivertjenestene 1. januar</strong><br />
+                    Onsdag 1. januar kl. 17:00 til 23:00 kan du ikke benytte arbeidsgivertjenestene
+                    på arbeidsplassen.no. Det er på grunn av nedetid i Altinn relatert til
+                    Kommune- og fylkessammenslåingen.
+                </div>
+            </AlertStripeAdvarsel>
+            <AlertStripeFeil className="blokk-xs alertstripe--solid Feilmelding__alertstripe">
+                <div>
+                    <strong>{overskrift}</strong><br />
+                    {melding}
+                </div>
+                <Hovedknapp mini onClick={refreshPage}>Last siden på nytt</Hovedknapp>
+            </AlertStripeFeil>
+        </div>
     );
 };
 
