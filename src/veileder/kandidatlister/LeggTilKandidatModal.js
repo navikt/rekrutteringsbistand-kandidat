3258c54a5b29a709f00ffbaa9f58ce367a962b26
Joar Aurdal <joar.aurdal@nav.no>
2020-02-28 12:31:43 +0100

    Endre tekst til henne i steden for hun

--- src/veileder/kandidatlister/LeggTilKandidatModal.js
+++ src/veileder/kandidatlister/LeggTilKandidatModal.js
@@ -146,7 +146,7 @@ class LeggTilKandidatModal extends React.Component {
             >
                 <Systemtittel className="tittel">Legg til kandidat</Systemtittel>
                 <AlertStripeAdvarsel>
-                    Avklar kandidaten før du legger han eller hun på kandidatlisten.
+                    Avklar kandidaten før du legger han eller henne på kandidatlisten.
                 </AlertStripeAdvarsel>
                 <Input
                     className="skjemaelement--pink legg-til-kandidat__fodselsnummer"
