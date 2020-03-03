12ebfa859fe813082f4bfa1b3a18e43e8dbf6a6b
André Mossige <mossige.a@gmail.com>
2020-03-03 10:31:54 +0100

    Prøv å legg til dsn igjen

--- src/veileder/app.js
+++ src/veileder/app.js
@@ -49,6 +49,7 @@ import * as Sentry from '@sentry/browser';
 import { getMiljø } from '../felles/common/miljøUtils';
 
 Sentry.init({
+    dsn: 'https://bd029fab6cab426eb0415b89a7f07124@sentry.gc.nav.no//20',
     environment: getMiljø(),
     enabled: getMiljø() === 'dev-fss' || getMiljø() === 'prod-fss',
     transportOptions: {
