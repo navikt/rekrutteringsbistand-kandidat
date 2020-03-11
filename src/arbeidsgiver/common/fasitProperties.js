491637966a5ba610034cd544a23ef116c48a6c3e
André Mossige <mossige.a@gmail.com>
2020-03-11 14:43:04 +0100

    Start å fjerne filer fra arbeidsgiver

--- src/arbeidsgiver/common/fasitProperties.js
+++ src/arbeidsgiver/common/fasitProperties.js
@@ -1,8 +1,2 @@
 /* eslint-disable no-undef */
 export const SEARCH_API = `${__PAM_KANDIDATSOK_API_URL__}kandidatsok/`;
-export const SAMTYKKE_API = `${__PAM_KANDIDATSOK_API_URL__}vilkar/`;
-export const KODEVERK_API = `${__PAM_KANDIDATSOK_API_URL__}kodeverk/`;
-export const KANDIDATLISTE_API = `${__PAM_KANDIDATSOK_API_URL__}arbeidsgiver/`;
-export const LOGIN_URL = __LOGIN_URL__;
-export const LOGOUT_URL = __LOGOUT_URL__;
-export const PAMPORTAL_URL = __PAMPORTAL_URL__;
--- src/arbeidsgiver/sok/api.ts
+++ src/arbeidsgiver/sok/api.ts
@@ -1,8 +1,4 @@
-/* eslint-disable no-underscore-dangle */
-
-import { KANDIDATLISTE_API, SEARCH_API, KODEVERK_API } from '../common/fasitProperties';
-import FEATURE_TOGGLES from '../../felles/konstanter';
-import { deleteReq, fetchJson, postJson, putJson } from '../../felles/api';
+import { SEARCH_API } from '../common/fasitProperties';
 
 const convertToUrlParams = query =>
     Object.keys(query)
@@ -24,56 +20,4 @@ export async function fetchTypeaheadSuggestionsRest(query = {}) {
         credentials: 'include',
     });
     return resultat.json();
-}
-
-export function fetchFeatureToggles() {
-    return fetchJson(`${SEARCH_API}toggles?feature=${FEATURE_TOGGLES.join(',')}`);
-}
-
-export function fetchKandidater(query = {}) {
-    return fetchJson(`${SEARCH_API}sok?${convertToUrlParams(query)}`, true);
-}
-
-export function fetchKandidaterES(query = {}) {
-    return fetchJson(`${SEARCH_API}sok?${convertToUrlParams(query)}`, true);
-}
-
-export function fetchCv(arenaKandidatnr) {
-    return fetchJson(`${SEARCH_API}hentcv?${convertToUrlParams(arenaKandidatnr)}`, true);
-}
-
-export function fetchKandidatlister(orgNummer) {
-    return fetchJson(`${KANDIDATLISTE_API}${orgNummer}/kandidatlister`, true);
-}
-
-export function postKandidatliste(kandidatlistBeskrivelse, orgNr) {
-    return postJson(
-        `${KANDIDATLISTE_API}${orgNr}/kandidatlister`,
-        JSON.stringify(kandidatlistBeskrivelse)
-    );
-}
-
-export function postKandidaterTilKandidatliste(kandidatlisteId, kandidater) {
-    return postJson(
-        `${KANDIDATLISTE_API}kandidatlister/${kandidatlisteId}/kandidater`,
-        JSON.stringify(kandidater)
-    );
-}
-
-export function putKandidatliste(kandidatlisteBeskrivelse) {
-    return putJson(
-        `${KANDIDATLISTE_API}kandidatlister/${kandidatlisteBeskrivelse.kandidatlisteId}`,
-        JSON.stringify(kandidatlisteBeskrivelse)
-    );
-}
-
-export function deleteKandidatliste(kandidatlisteId) {
-    return deleteReq(
-        `${KANDIDATLISTE_API}kandidatlister/${kandidatlisteId}`,
-        JSON.stringify(kandidatlisteId)
-    );
-}
-
-export function fetchGeografiKode(geografiKode) {
-    return fetchJson(`${KODEVERK_API}arenageografikoder/${geografiKode}`, true);
 }
