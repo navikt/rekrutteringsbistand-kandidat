cae383ac68e10f1bde279ffb30d7115288c1ff0c
Joar Aurdal <joar.aurdal@nav.no>
2020-03-25 09:30:53 +0100

    legg til styling lenker kritiskeyrker

--- src/veileder/result/kritiskeyrker/Bransjevelger.tsx
+++ src/veileder/result/kritiskeyrker/Bransjevelger.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { Bransje, Sok } from './Bransje';
 import { Normaltekst, Element } from 'nav-frontend-typografi';
+import Lenke from 'nav-frontend-lenker';
 import './Bransjevelger.less';
 
 interface BransjevelgerProps {
@@ -11,6 +12,10 @@ interface BransjevelgerProps {
 
 const linktekst = (sok: Sok) => {
     return `${sok.tittel} (${sok.antallTreff})`;
+};
+
+const linkurl = (sok: Sok) => {
+    return '#';
 };
 
 const Bransjevelger = (props: BransjevelgerProps) => {
@@ -23,12 +28,12 @@ const Bransjevelger = (props: BransjevelgerProps) => {
                         <div key={_.tittel} className="bransjevelger__bransje__innhold">
                             <Element>{_.tittel}</Element>
                             <Normaltekst>Se kandidater som har:</Normaltekst>
+
                             {_.sok.map(_ => (
-                                <Normaltekst className="bransjevelger__linktekst" key={_.tittel}>
-                                    {linktekst(_)}
-                                </Normaltekst>
+                                    <Lenke href={linkurl(_)} key={_.tittel}>
+                                        {linktekst(_)}
+                                    </Lenke>
                             ))}
-                            <div></div>
                         </div>
                     ))}
                 </div>
