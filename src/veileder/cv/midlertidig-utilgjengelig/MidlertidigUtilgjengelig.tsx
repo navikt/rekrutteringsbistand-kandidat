5871fbd0d5dab545b435f458c6f08c6413ac23c8
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-04-08 14:13:58 +0200

    Juster padding i popover

--- src/veileder/cv/midlertidig-utilgjengelig/MidlertidigUtilgjengelig.tsx
+++ src/veileder/cv/midlertidig-utilgjengelig/MidlertidigUtilgjengelig.tsx
@@ -3,7 +3,7 @@ import { Knapp } from 'pam-frontend-knapper';
 import Popover, { PopoverOrientering } from 'nav-frontend-popover';
 import './MidlertidigUtilgjengelig.less';
 import Chevron from 'nav-frontend-chevron';
-import { Normaltekst, Systemtittel, Element } from 'nav-frontend-typografi';
+import { Normaltekst, Systemtittel, Element, Undertittel } from 'nav-frontend-typografi';
 
 interface Props {}
 
@@ -23,17 +23,24 @@ const MidlertidigUtilgjengelig: FunctionComponent<Props> = props => {
                 ankerEl={anker}
                 onRequestClose={() => setAnker(undefined)}
                 orientering={PopoverOrientering.UnderHoyre}
+                avstandTilAnker={16}
             >
                 <div className="midlertidig-utilgjengelig__popup-innhold">
-                    <Systemtittel>Registrer som midlertidig utilgjengelig</Systemtittel>
+                    <Undertittel tag="h2">Registrer som midlertidig utilgjengelig</Undertittel>
                     <Normaltekst>
                         Avklar tilgjengeligheten. Gi beskjed til kandidaten hvis du registrerer
                         «midlertidig utilgjengelig».
                     </Normaltekst>
-                    <Element>Hvor lenge er kandidaten utilgjengelig?*</Element>
-                    <Normaltekst>Du kan velge maks en måned frem i tid</Normaltekst>
+                    <div className="midlertidig-utilgjengelig__datovelger">
+                        <Element>Hvor lenge er kandidaten utilgjengelig?*</Element>
+                        <Normaltekst>Du kan velge maks en måned frem i tid</Normaltekst>
+                    </div>
                     <Knapp type="hoved">Lagre</Knapp>
-                    <Knapp type="flat" onClick={() => setAnker(undefined)}>
+                    <Knapp
+                        type="flat"
+                        className="midlertidig-utilgjengelig__avbryt"
+                        onClick={() => setAnker(undefined)}
+                    >
                         Avbryt
                     </Knapp>
                 </div>
