b60edc3814ece2bc58a62b1df62cb19e2526b9ce
André Mossige <mossige.a@gmail.com>
2020-04-20 14:00:56 +0200

    Flytt formattering til egen fil

--- src/veileder/cv/VisKandidatPersonalia.js
+++ src/veileder/cv/VisKandidatPersonalia.js
@@ -2,46 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Column, Container } from 'nav-frontend-grid';
 import { Normaltekst } from 'nav-frontend-typografi';
-import { formatterDato } from '../../felles/common/dateUtils';
 import cvPropTypes from '../../felles/PropTypes';
 import TelefonIkon from '../../felles/common/ikoner/TelefonIkon';
 import MailIkon from '../../felles/common/ikoner/MailIkon';
 import AdresseIkon from '../../felles/common/ikoner/AdresseIkon';
 import VisKandidatForrigeNeste from './VisKandidatForrigeNeste';
-import { capitalizeFirstLetter, capitalizePoststed } from '../../felles/sok/utils';
+import { capitalizeFirstLetter } from '../../felles/sok/utils';
 import { LenkeMedChevron } from './lenkeMedChevron/LenkeMedChevron.tsx';
 import Sidetittel from '../../felles/common/Sidetittel.tsx';
-
-const formaterFødselsdato = (fodselsdato, fodselsnummer) => {
-    if (fodselsdato) {
-        return `Fødselsdato: ${formatterDato(new Date(fodselsdato))}${
-            fodselsnummer && ` (${fodselsnummer})`
-        }`;
-    } else if (fodselsnummer) {
-        return `Fødselsnummer: ${fodselsnummer}`;
-    }
-    return '';
-};
+import {
+    formaterFødselsdato,
+    formatMobileTelephoneNumber,
+    formatterAdresse,
+} from './personaliaFormattering';
 
 class VisKandidatPersonalia extends React.Component {
-    formatMobileTelephoneNumber = (inputString) => {
-        const inputStringNoWhiteSpace = inputString.replace(/\s/g, '');
-        const actualNumber = inputStringNoWhiteSpace.slice(-8);
-        const countryCode = inputStringNoWhiteSpace.slice(-99, -8);
-        const lastString = actualNumber.slice(-3);
-        const midString = actualNumber.slice(-5, -3);
-        const firstString = actualNumber.slice(-8, -5);
-
-        return `${countryCode} ${firstString} ${midString} ${lastString}`;
-    };
-
-    formatterAdresse = (gate, postnummer, poststed) => {
-        const sisteDel = [postnummer, poststed ? capitalizePoststed(poststed) : null]
-            .filter((string) => string)
-            .join(' ');
-        return [gate, sisteDel].filter((string) => string).join(', ');
-    };
-
     render() {
         const {
             cv,
@@ -126,9 +101,7 @@ class VisKandidatPersonalia extends React.Component {
                                         <TelefonIkon color="#3E3832" />
                                     </div>
                                     <Normaltekst className="header--personalia__tekst">
-                                        <strong>
-                                            {this.formatMobileTelephoneNumber(cv.telefon)}
-                                        </strong>
+                                        <strong>{formatMobileTelephoneNumber(cv.telefon)}</strong>
                                     </Normaltekst>
                                 </div>
                             )}
@@ -138,7 +111,7 @@ class VisKandidatPersonalia extends React.Component {
                                         <AdresseIkon color="#3E3832" />
                                     </div>
                                     <Normaltekst className="header--personalia__tekst">
-                                        {this.formatterAdresse(
+                                        {formatterAdresse(
                                             cv.adresse.adrlinje1,
                                             cv.adresse.postnr,
                                             cv.adresse.poststednavn
