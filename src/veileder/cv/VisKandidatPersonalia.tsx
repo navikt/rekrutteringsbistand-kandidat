0ec93235e1ce77fcf3efdddf7177c0e506a57367
André Mossige <mossige.a@gmail.com>
2020-04-20 14:18:24 +0200

    Gjør om VisKandidatPersonalia til FunctionalComponent

--- src/veileder/cv/VisKandidatPersonalia.tsx
+++ src/veileder/cv/VisKandidatPersonalia.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { Column, Container } from 'nav-frontend-grid';
 import { Normaltekst } from 'nav-frontend-typografi';
 import TelefonIkon from '../../felles/common/ikoner/TelefonIkon';
@@ -24,114 +24,110 @@ interface Props {
     fantCv: boolean;
 }
 
-class VisKandidatPersonalia extends React.Component<Props> {
-    render() {
-        const {
-            cv,
-            antallKandidater,
-            tilbakeLink,
-            gjeldendeKandidatIndex,
-            forrigeKandidat,
-            nesteKandidat,
-            fantCv,
-        } = this.props;
-
-        let fornavnStorForbokstav;
-        if (cv.fornavn) {
-            fornavnStorForbokstav = capitalizeFirstLetter(cv.fornavn);
-        }
-        let etternavnStorForbokstav;
-        if (cv.etternavn) {
-            etternavnStorForbokstav = capitalizeFirstLetter(cv.etternavn);
-        }
-
-        const lenkeClass = 'header--personalia__lenke--veileder';
-        let lenkeText = 'Til kandidatsøket';
-        if (tilbakeLink.includes('kandidater/lister')) {
-            lenkeText = 'Til kandidatlisten';
-        } else if (tilbakeLink.includes('kandidater-next')) {
-            lenkeText = 'Til liste kandidatmatch';
-        }
-        return (
-            <div className="header--bakgrunn__veileder" id="bakgrunn-personalia">
-                <Container className="blokk-s">
-                    <Column className="header--personalia__lenker--container">
-                        <LenkeMedChevron
-                            type="venstre"
-                            to={tilbakeLink}
-                            className={lenkeClass}
-                            text={lenkeText}
-                        />
-                        {fantCv && (
-                            <VisKandidatForrigeNeste
-                                lenkeClass={lenkeClass}
-                                forrigeKandidat={forrigeKandidat}
-                                nesteKandidat={nesteKandidat}
-                                gjeldendeKandidatIndex={gjeldendeKandidatIndex}
-                                antallKandidater={antallKandidater}
-                            />
-                        )}
-                    </Column>
-                </Container>
-
-                <div>
-                    <Sidetittel className="header--personalia__overskrift">
-                        {fantCv
-                            ? `${fornavnStorForbokstav} ${etternavnStorForbokstav}`
-                            : 'Informasjonen om kandidaten kan ikke vises'}
-                    </Sidetittel>
-                    <Normaltekst className="header--personalia__fodselsdato">
-                        {formaterFødselsdato(cv.fodselsdato, cv.fodselsnummer)}
-                    </Normaltekst>
-                </div>
-                {fantCv && (
-                    <div>
-                        <div className="personalia-container">
-                            {cv.epost && (
-                                <div className="personalia--item">
-                                    <div className="personalia--icon">
-                                        <MailIkon color="#3E3832" />
-                                    </div>
-                                    <Normaltekst className="header--personalia__tekst">
-                                        <a
-                                            href={`mailto:${cv.epost}`}
-                                            className="header--personalia__mail--veileder"
-                                        >
-                                            {cv.epost}
-                                        </a>
-                                    </Normaltekst>
-                                </div>
-                            )}
-                            {cv.telefon && (
-                                <div className="personalia--item">
-                                    <div className="personalia--icon">
-                                        <TelefonIkon color="#3E3832" />
-                                    </div>
-                                    <Normaltekst className="header--personalia__tekst">
-                                        <strong>{formatMobileTelephoneNumber(cv.telefon)}</strong>
-                                    </Normaltekst>
-                                </div>
-                            )}
-                            {cv.adresse && cv.adresse.adrlinje1 && (
-                                <div className="personalia--item">
-                                    <div className="personalia--icon">
-                                        <AdresseIkon color="#3E3832" />
-                                    </div>
-                                    <Normaltekst className="header--personalia__tekst">
-                                        {formatterAdresse(
-                                            cv.adresse.adrlinje1,
-                                            cv.adresse.postnr,
-                                            cv.adresse.poststednavn
-                                        )}
-                                    </Normaltekst>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                )}
-            </div>
-        );
+const VisKandidatPersonalia: FunctionComponent<Props> = ({
+    cv,
+    antallKandidater,
+    tilbakeLink,
+    gjeldendeKandidatIndex,
+    forrigeKandidat,
+    nesteKandidat,
+    fantCv,
+}) => {
+    let fornavnStorForbokstav;
+    if (cv.fornavn) {
+        fornavnStorForbokstav = capitalizeFirstLetter(cv.fornavn);
     }
-}
+    let etternavnStorForbokstav;
+    if (cv.etternavn) {
+        etternavnStorForbokstav = capitalizeFirstLetter(cv.etternavn);
+    }
+
+    const lenkeClass = 'header--personalia__lenke--veileder';
+    let lenkeText = 'Til kandidatsøket';
+    if (tilbakeLink.includes('kandidater/lister')) {
+        lenkeText = 'Til kandidatlisten';
+    } else if (tilbakeLink.includes('kandidater-next')) {
+        lenkeText = 'Til liste kandidatmatch';
+    }
+    return (
+        <div className="header--bakgrunn__veileder" id="bakgrunn-personalia">
+            <Container className="blokk-s">
+                <Column className="header--personalia__lenker--container">
+                    <LenkeMedChevron
+                        type="venstre"
+                        to={tilbakeLink}
+                        className={lenkeClass}
+                        text={lenkeText}
+                    />
+                    {fantCv && (
+                        <VisKandidatForrigeNeste
+                            lenkeClass={lenkeClass}
+                            forrigeKandidat={forrigeKandidat}
+                            nesteKandidat={nesteKandidat}
+                            gjeldendeKandidatIndex={gjeldendeKandidatIndex}
+                            antallKandidater={antallKandidater}
+                        />
+                    )}
+                </Column>
+            </Container>
+
+            <div>
+                <Sidetittel className="header--personalia__overskrift">
+                    {fantCv
+                        ? `${fornavnStorForbokstav} ${etternavnStorForbokstav}`
+                        : 'Informasjonen om kandidaten kan ikke vises'}
+                </Sidetittel>
+                <Normaltekst className="header--personalia__fodselsdato">
+                    {formaterFødselsdato(cv.fodselsdato, cv.fodselsnummer)}
+                </Normaltekst>
+            </div>
+            {fantCv && (
+                <div>
+                    <div className="personalia-container">
+                        {cv.epost && (
+                            <div className="personalia--item">
+                                <div className="personalia--icon">
+                                    <MailIkon color="#3E3832" />
+                                </div>
+                                <Normaltekst className="header--personalia__tekst">
+                                    <a
+                                        href={`mailto:${cv.epost}`}
+                                        className="header--personalia__mail--veileder"
+                                    >
+                                        {cv.epost}
+                                    </a>
+                                </Normaltekst>
+                            </div>
+                        )}
+                        {cv.telefon && (
+                            <div className="personalia--item">
+                                <div className="personalia--icon">
+                                    <TelefonIkon color="#3E3832" />
+                                </div>
+                                <Normaltekst className="header--personalia__tekst">
+                                    <strong>{formatMobileTelephoneNumber(cv.telefon)}</strong>
+                                </Normaltekst>
+                            </div>
+                        )}
+                        {cv.adresse && cv.adresse.adrlinje1 && (
+                            <div className="personalia--item">
+                                <div className="personalia--icon">
+                                    <AdresseIkon color="#3E3832" />
+                                </div>
+                                <Normaltekst className="header--personalia__tekst">
+                                    {formatterAdresse(
+                                        cv.adresse.adrlinje1,
+                                        cv.adresse.postnr,
+                                        cv.adresse.poststednavn
+                                    )}
+                                </Normaltekst>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default VisKandidatPersonalia;
