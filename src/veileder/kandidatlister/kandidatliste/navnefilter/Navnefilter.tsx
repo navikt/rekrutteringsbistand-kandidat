6806a615b5df0df4ee56b0b40f511178f4d0b978
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-04-24 14:39:46 +0200

    Bedre søkeikon, accessibility

--- src/veileder/kandidatlister/kandidatliste/navnefilter/Navnefilter.tsx
+++ src/veileder/kandidatlister/kandidatliste/navnefilter/Navnefilter.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import Søkeikon from './Søkeikon';
 import './Navnefilter.less';
 
 interface Props {
@@ -9,15 +10,17 @@ interface Props {
 
 const Navnefilter: FunctionComponent<Props> = ({ value, onChange, onReset }) => {
     const tittel = 'Søk etter navn i listen';
-    const knappetekst = 'Fjern filter ';
+    const knappetekst = 'Fjern filter';
 
     return (
-        <>
+        <div className="navnefilter">
+            <Søkeikon />
+            <label className="navnefilter__label">Søk etter navn i listen</label>
             <input
                 placeholder={tittel}
                 value={value}
                 onChange={onChange}
-                className="navnefilter navnefilter__søkeikon"
+                className="navnefilter__input navnefilter__søkeikon"
             />
             {value.length > 0 && (
                 <button
@@ -26,7 +29,7 @@ const Navnefilter: FunctionComponent<Props> = ({ value, onChange, onReset }) => 
                     onClick={onReset}
                 />
             )}
-        </>
+        </div>
     );
 };
 
