8d85d4361ec47fcd16a34e418ab1bd2bf327cdbd
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-04-27 09:57:45 +0200

    Diverse finpuss på navnefilter

--- src/veileder/kandidatlister/kandidatliste/navnefilter/Søkeikon.tsx
+++ src/veileder/kandidatlister/kandidatliste/navnefilter/Søkeikon.tsx
@@ -5,7 +5,7 @@ const Søkeikon: FunctionComponent = () => (
         <path
             d="M7.6 14.4c1.7 0 3.3-.6 4.5-1.6l6.1 6.2c.3.3.8.3 1.2 0 .3-.3.3-.9 0-1.2l-6.2-6.1a7.2 7.2 0 10-5.6 2.7zm0-12.8a5.6 5.6 0 110 11.2 5.6 5.6 0 010-11.2z"
             fill="#3E3832"
-            fill-rule="evenodd"
+            fillRule="evenodd"
         />
     </svg>
 );
