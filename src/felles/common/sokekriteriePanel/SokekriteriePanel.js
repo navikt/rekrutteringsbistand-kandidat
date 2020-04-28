6b5ab1f62e0f071432485b97a2cab2a77cb7ba98
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-04-28 13:29:24 +0200

    Logg events rundt midlertidig utilgjengelig

--- src/felles/common/sokekriteriePanel/SokekriteriePanel.js
+++ src/felles/common/sokekriteriePanel/SokekriteriePanel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import NavFrontendChevron from 'nav-frontend-chevron';
 
-import { amplitudeClient, logEvent } from '../../../veileder/amplitude/amplitude';
+import { logEvent } from '../../../veileder/amplitude/amplitude';
 import './SokekriteriePanel.less';
 
 const SokekriteriePanel = ({ children, tittel, fane, apen, onClick, id }) => {
