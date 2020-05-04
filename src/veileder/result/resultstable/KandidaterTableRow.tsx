e53afe64dcda25018f915b0cac2b2217727c8037
André Mossige <mossige.a@gmail.com>
2020-05-04 11:38:33 +0200

    Gjør om KandidaterTableRow til FunctionComponent

--- src/veileder/result/resultstable/KandidaterTableRow.tsx
+++ src/veileder/result/resultstable/KandidaterTableRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './Resultstable.less';
@@ -17,12 +17,12 @@ interface Props {
     stillingsId: string;
 }
 
-class KandidaterTableRow extends React.Component<Props> {
-    onCheck = (kandidatnr) => {
-        this.props.onKandidatValgt(!this.props.markert, kandidatnr);
+const KandidaterTableRow: FunctionComponent<Props> = (props) => {
+    const onCheck = (kandidatnr) => {
+        props.onKandidatValgt(!props.markert, kandidatnr);
     };
 
-    checkedClass = (markert, nettoppValgt) => {
+    const checkedClass = (markert, nettoppValgt) => {
         if (nettoppValgt) {
             return 'nettopp-valgt';
         } else if (markert) {
@@ -31,76 +31,74 @@ class KandidaterTableRow extends React.Component<Props> {
         return '';
     };
 
-    render() {
-        const {
-            kandidat,
-            markert = false,
-            nettoppValgt,
-            setScrollPosition,
-            kandidatlisteId,
-            stillingsId,
-        } = this.props;
-        const kandidatnummer = kandidat.arenaKandidatnr;
-        const fornavn = kandidat.fornavn ? capitalizeFirstLetter(kandidat.fornavn) : '';
-        const etternavn = kandidat.etternavn ? capitalizeFirstLetter(kandidat.etternavn) : '';
-        const navn = `${etternavn}, ${fornavn}`;
-        const fodselsnummer = kandidat.fodselsnummer;
-        const innsatsgruppe = kandidat.servicebehov;
-        const bosted = kandidat.poststed ? capitalizePoststed(kandidat.poststed) : '-';
-        const linkTilKandidat = () => {
-            if (kandidatlisteId) {
-                return `/kandidater/kandidatliste/${kandidatlisteId}/cv?kandidatNr=${kandidatnummer}`;
-            } else if (stillingsId) {
-                return `/kandidater/stilling/${stillingsId}/cv?kandidatNr=${kandidatnummer}`;
-            }
-            return `/kandidater/cv?kandidatNr=${kandidatnummer}`;
-        };
+    const {
+        kandidat,
+        markert = false,
+        nettoppValgt,
+        setScrollPosition,
+        kandidatlisteId,
+        stillingsId,
+    } = props;
+    const kandidatnummer = kandidat.arenaKandidatnr;
+    const fornavn = kandidat.fornavn ? capitalizeFirstLetter(kandidat.fornavn) : '';
+    const etternavn = kandidat.etternavn ? capitalizeFirstLetter(kandidat.etternavn) : '';
+    const navn = `${etternavn}, ${fornavn}`;
+    const fodselsnummer = kandidat.fodselsnummer;
+    const innsatsgruppe = kandidat.servicebehov;
+    const bosted = kandidat.poststed ? capitalizePoststed(kandidat.poststed) : '-';
+    const linkTilKandidat = () => {
+        if (kandidatlisteId) {
+            return `/kandidater/kandidatliste/${kandidatlisteId}/cv?kandidatNr=${kandidatnummer}`;
+        } else if (stillingsId) {
+            return `/kandidater/stilling/${stillingsId}/cv?kandidatNr=${kandidatnummer}`;
+        }
+        return `/kandidater/cv?kandidatNr=${kandidatnummer}`;
+    };
 
-        return (
-            <div className={`NyKandidaterTableRow ${this.checkedClass(markert, nettoppValgt)}`}>
-                <div className="kandidat-content">
-                    <div className="kolonne-checkbox skjemaelement--pink">
-                        <div className="skjemaelement skjemaelement--horisontal text-hide">
-                            <input
-                                type="checkbox"
-                                id={`marker-kandidat-${kandidatnummer}-checkbox`}
-                                className="skjemaelement__input checkboks"
-                                aria-label={`Marker kandidat med navn ${navn}`}
-                                checked={markert}
-                                onChange={() => {
-                                    this.onCheck(kandidat.arenaKandidatnr);
-                                }}
-                            />
-                            <label
-                                className="skjemaelement__label"
-                                htmlFor={`marker-kandidat-${kandidatnummer}-checkbox`}
-                                aria-hidden="true"
-                            >
-                                .
-                            </label>
-                        </div>
-                    </div>
-                    <div className="NyKandidaterTableRow__tilgjengelighet">
-                        <TilgjengelighetFlagg status={kandidat.midlertidigUtilgjengeligStatus} />
-                    </div>
-                    <div className="kolonne-navn kolonne-tekst">
-                        <Link
-                            className="kolonne-lenke link"
-                            to={linkTilKandidat()}
-                            onClick={() => setScrollPosition(window.pageYOffset)}
-                            aria-label={`Se CV for ${navn}`}
+    return (
+        <div className={`NyKandidaterTableRow ${checkedClass(markert, nettoppValgt)}`}>
+            <div className="kandidat-content">
+                <div className="kolonne-checkbox skjemaelement--pink">
+                    <div className="skjemaelement skjemaelement--horisontal text-hide">
+                        <input
+                            type="checkbox"
+                            id={`marker-kandidat-${kandidatnummer}-checkbox`}
+                            className="skjemaelement__input checkboks"
+                            aria-label={`Marker kandidat med navn ${navn}`}
+                            checked={markert}
+                            onChange={() => {
+                                onCheck(kandidat.arenaKandidatnr);
+                            }}
+                        />
+                        <label
+                            className="skjemaelement__label"
+                            htmlFor={`marker-kandidat-${kandidatnummer}-checkbox`}
+                            aria-hidden="true"
                         >
-                            {navn}
-                        </Link>
+                            .
+                        </label>
                     </div>
-                    <div className="kolonne-dato kolonne-tekst">{fodselsnummer}</div>
-                    <div className="kolonne-innsatsgruppe kolonne-tekst">{innsatsgruppe}</div>
-                    <div className="kolonne-bosted kolonne-tekst">{bosted}</div>
                 </div>
+                <div className="NyKandidaterTableRow__tilgjengelighet">
+                    <TilgjengelighetFlagg status={kandidat.midlertidigUtilgjengeligStatus} />
+                </div>
+                <div className="kolonne-navn kolonne-tekst">
+                    <Link
+                        className="kolonne-lenke link"
+                        to={linkTilKandidat()}
+                        onClick={() => setScrollPosition(window.pageYOffset)}
+                        aria-label={`Se CV for ${navn}`}
+                    >
+                        {navn}
+                    </Link>
+                </div>
+                <div className="kolonne-dato kolonne-tekst">{fodselsnummer}</div>
+                <div className="kolonne-innsatsgruppe kolonne-tekst">{innsatsgruppe}</div>
+                <div className="kolonne-bosted kolonne-tekst">{bosted}</div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 const mapDispatchToProps = (dispatch) => ({
     setScrollPosition: (scrollPosisjon) =>
