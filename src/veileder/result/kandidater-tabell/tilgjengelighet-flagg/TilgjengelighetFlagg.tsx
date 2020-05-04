f4bd40ccce97139212a0ecfae48a381d10aa6798
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-05-04 15:54:57 +0200

    Vis popover ved klikk på tilgjengelighetsflagg

--- src/veileder/result/kandidater-tabell/tilgjengelighet-flagg/TilgjengelighetFlagg.tsx
+++ src/veileder/result/kandidater-tabell/tilgjengelighet-flagg/TilgjengelighetFlagg.tsx
@@ -1,13 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Tilgjengelighet } from '../../../sok/tilgjengelighet/midlertidig-utilgjengelig/MidlertidigUtilgjengeligSearch';
 import TilgjengelighetIkon from '../../../cv/midlertidig-utilgjengelig/tilgjengelighet-ikon/TilgjengelighetIkon';
+import Popover, { PopoverOrientering } from 'nav-frontend-popover';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
 import './TilgjengelighetFlagg.less';
 
 interface Props {
     status: Tilgjengelighet;
 }
 
+const hentPopovertittel = (tilgjengelighet: Tilgjengelighet) => {
+    switch (tilgjengelighet) {
+        case Tilgjengelighet.TilgjengeligInnen1Uke:
+            return 'Tilgjengelig innen 1 uke';
+        case Tilgjengelighet.MidlertidigUtilgjengelig:
+            return 'Midlertidig utilgjengelig';
+    }
+};
+
 const TilgjengelighetFlagg: FunctionComponent<Props> = ({ status }) => {
+    const [anker, setAnker] = useState<any>(undefined);
+
     let midlertidigUtilgjengeligFlagg;
     if (status === Tilgjengelighet.Tilgjengelig) {
         midlertidigUtilgjengeligFlagg = null;
@@ -29,7 +42,37 @@ const TilgjengelighetFlagg: FunctionComponent<Props> = ({ status }) => {
         return null;
     }
 
-    return midlertidigUtilgjengeligFlagg;
+    const togglePopover = (e: any) => {
+        setAnker(!anker ? e.currentTarget : undefined);
+    };
+
+    const lukkPopover = () => {
+        setAnker(undefined);
+    };
+
+    return (
+        <>
+            <button className="tilgjengelighet-flagg__knapp" onClick={togglePopover}>
+                {midlertidigUtilgjengeligFlagg}
+            </button>
+            {midlertidigUtilgjengeligFlagg !== null && (
+                <div className="tilgjengelighet-flagg__popover">
+                    <Popover
+                        ankerEl={anker}
+                        onRequestClose={lukkPopover}
+                        orientering={PopoverOrientering.Under}
+                    >
+                        <div className="tilgjengelighet-flagg__popover-innhold">
+                            <Element>{hentPopovertittel(status)}</Element>
+                            <Normaltekst>
+                                Tilgjengelig om: <b>... dager</b> (...)
+                            </Normaltekst>
+                        </div>
+                    </Popover>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default TilgjengelighetFlagg;
