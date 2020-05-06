b870f2efadc35175a05fd9b1b4846885e0062577
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-05-06 16:06:20 +0200

    Fjern fokus ved click

--- src/veileder/common/tilToppenKnapp/TilToppenKnapp.tsx
+++ src/veileder/common/tilToppenKnapp/TilToppenKnapp.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TilToppenKnapp.less';
 import { Knapp } from 'nav-frontend-knapper';
 import throttle from 'lodash.throttle';
@@ -6,10 +6,20 @@ import classNames from 'classnames';
 
 export const TilToppenKnapp = () => {
     const [scrollPosition, setScrollPosition] = useState<number | undefined>();
+    const knappRef = useRef<HTMLButtonElement>(null);
 
     const onScroll = throttle(() => {
         setScrollPosition(window.scrollY);
     }, 1000);
+
+    const onClick = (e) => {
+        if (knappSkalVises) {
+            window.scrollTo({ top: 0 });
+        }
+        if (knappRef && knappRef.current) {
+            knappRef.current.blur();
+        }
+    };
 
     useEffect(() => {
         window.addEventListener('scroll', onScroll);
@@ -22,14 +32,16 @@ export const TilToppenKnapp = () => {
     const knappSkalVises = scrollPosition && scrollPosition > window.innerHeight;
 
     return (
-        <Knapp
-            type="hoved"
+        <button
+            ref={knappRef}
             className={classNames(
                 'tilToppenKnapp',
+                'knapp',
+                'knapp--hoved',
                 !knappSkalVises && 'tilToppenKnapp--skjul'
             )}
             aria-hidden={!knappSkalVises}
-            onClick={() => knappSkalVises && window.scrollTo({ top: 0 })}
+            onClick={onClick}
         >
             <svg width="33" height="36" viewBox="0 0 22 24" xmlns="http://www.w3.org/2000/svg">
                 <title>Til toppen</title>
@@ -38,6 +50,6 @@ export const TilToppenKnapp = () => {
                     fillRule="evenodd"
                 />
             </svg>
-        </Knapp>
+        </button>
     );
 };
