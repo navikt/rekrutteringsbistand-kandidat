308d31ac8db576123aeb34da7b31b31ce2aa90a9
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-05-13 08:49:25 +0200

    Lag ny styling til kandidatlisten

--- src/veileder/kandidatlister/kandidatliste/side-header/SideHeader.tsx
+++ src/veileder/kandidatlister/kandidatliste/side-header/SideHeader.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react';
-import { Sidetittel, Element, Normaltekst } from 'nav-frontend-typografi';
+import { Sidetittel, Element } from 'nav-frontend-typografi';
+import Lenke from 'nav-frontend-lenker';
+
 import { OpprettetAv } from '../../kandidatlistetyper';
 import { capitalizeEmployerName } from '../../../../felles/sok/utils';
 import './SideHeader.less';
@@ -13,14 +15,15 @@ type Props = {
     beskrivelse?: string;
 };
 
-const SideHeader: FunctionComponent<Props> = ({
-    tittel,
-    antallKandidater,
-    arbeidsgiver,
-    opprettetAv,
-    stillingsId,
-    beskrivelse,
-}) => (
+const SideHeader: FunctionComponent<Props> = (
+    {
+        tittel,
+        antallKandidater,
+        arbeidsgiver,
+        opprettetAv,
+        stillingsId,
+        beskrivelse,
+    } /*
     <div className="side-header">
         <div className="side-header__wrapper">
             <div className="side-header__top">
@@ -62,7 +65,30 @@ const SideHeader: FunctionComponent<Props> = ({
                 </div>
             )}
         </div>
-    </div>
+    </div>*/
+) => (
+    <header className="side-header">
+        <div className="side-header__inner">
+            <div className="side-header__tilbake">Til kandidatlisten</div>
+            <div className="side-header__informasjon">
+                <Sidetittel className="side-header__tittel">{tittel}</Sidetittel>
+                <Element className="side-header__antall-kandidater">
+                    {antallKandidater === 1 ? '1 kandidat' : `${antallKandidater} kandidater`}
+                </Element>
+                <div className="side-header__om-kandidatlisten">
+                    {arbeidsgiver && (
+                        <span>Arbeidsgiver: {capitalizeEmployerName(arbeidsgiver)}</span>
+                    )}
+                    <span>
+                        Registrert av: {opprettetAv.navn} ({opprettetAv.ident})
+                    </span>
+                    {stillingsId && (
+                        <Lenke href={`/stilling/${stillingsId}`}>Se stillingsannonse</Lenke>
+                    )}
+                </div>
+            </div>
+        </div>
+    </header>
 );
 
 export default SideHeader;
