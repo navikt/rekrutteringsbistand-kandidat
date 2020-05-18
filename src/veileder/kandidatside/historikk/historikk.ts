a558acf720a0a5c695ac39e21ca14279a590bf93
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-05-18 10:57:44 +0200

    Legg til api-kall for kandidats kandidatlister

---
+++ src/veileder/kandidatside/historikk/historikk.ts
@@ -0,0 +1,17 @@
+interface KandidatlisteForKandidat {
+     kandidatnr: string;
+     fornavn: string;
+     etternavn: string;
+     lagtTilTidspunkt: string;
+     lagtTilAvIdent: string;
+     lagtTilAvEpost: string;
+     lagtTilAvNavn: string;
+     status: string;
+     utfall: string;
+     uuid: string;
+     tittel: string;
+     organisasjonReferanse: string;
+     organisasjonNavn: string;
+     stillingId: string;
+     slettet: boolean;
+}
