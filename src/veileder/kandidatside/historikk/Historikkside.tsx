28d9bc9cc9c5c900bd436577c6256709f117a9bb
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-05-19 14:47:39 +0200

    Legg til crude tabell

--- src/veileder/kandidatside/historikk/Historikkside.tsx
+++ src/veileder/kandidatside/historikk/Historikkside.tsx
@@ -4,11 +4,14 @@ import { KandidatlisterForKandidatActionType } from './historikkReducer';
 import { Nettstatus } from '../../../felles/common/remoteData';
 import { useRouteMatch } from 'react-router-dom';
 import AppState from '../../AppState';
+import 'nav-frontend-tabell-style';
+import './Historikkside.less';
 
 const Historikkside: FunctionComponent = () => {
     const { params } = useRouteMatch<{ kandidatnr: string }>();
     const kandidatnr = params.kandidatnr;
     const historikk = useSelector((state: AppState) => state.historikk);
+    const cv = useSelector((state: AppState) => state.cv);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -18,14 +21,45 @@ const Historikkside: FunctionComponent = () => {
         });
     }, [kandidatnr, dispatch]);
 
-    if (historikk.kandidatlisterForKandidat.kind !== Nettstatus.Suksess) {
+    if (
+        historikk.kandidatlisterForKandidat.kind !== Nettstatus.Suksess ||
+        cv.hentStatus !== 'SUCCESS'
+    ) {
         return null;
     }
 
     const kandidatlister = historikk.kandidatlisterForKandidat.data;
     return (
-        <div>
+        <div className="historikkside">
             <h2>Historikk</h2>
+
+            <table className="tabell tabell--stripet">
+                <thead>
+                    <th>Lagt i listen</th>
+                    <th>Navn på kandidatliste</th>
+                    <th>Arbeidsgiver</th>
+                    <th>Lagt til av</th>
+                    <th>Status</th>
+                    <th>Utfall</th>
+                    <th>Stilling</th>
+                </thead>
+                <tbody>
+                    {kandidatlister.map((liste) => (
+                        <tr>
+                            <td>{liste.lagtTilTidspunkt}</td>
+                            <td>{liste.tittel}</td>
+                            <td>{liste.organisasjonNavn}</td>
+                            <td>
+                                {liste.lagtTilAvNavn} ({liste.lagtTilAvIdent})
+                            </td>
+                            <td>{liste.status}</td>
+                            <td>{liste.utfall}</td>
+                            <td>{liste.stillingId}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
             <ul>
                 {kandidatlister.map((liste) => (
                     <li key={liste.uuid}>
