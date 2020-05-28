af152171326f17960faf3ea006f41e3b0bbb8b7f
Are Husby <are.husby@nav.no>
2020-05-28 12:54:19 +0200

    Smalere + jukset til space mellom teskt og lenke

--- src/veileder/kandidatlister/kandidatliste/side-header/ListeBeskrivelse.tsx
+++ src/veileder/kandidatlister/kandidatliste/side-header/ListeBeskrivelse.tsx
@@ -1,5 +1,5 @@
-import React, {FunctionComponent, useState} from 'react';
-import {Element, Normaltekst} from 'nav-frontend-typografi';
+import React, { FunctionComponent, useState } from 'react';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
 import '../kandidatrad/Kandidatrad.less';
 import Lenkeknapp from '../../../../felles/common/Lenkeknapp';
 import NavFrontendChevron from 'nav-frontend-chevron';
@@ -20,10 +20,13 @@ const ListeBeskrivelse: FunctionComponent<Props> = ({ beskrivelse }) => {
                     ? beskrivelse
                     : beskrivelse.substr(0, antTegnSomAlltidVises) + ' ...'}
                 {beskrivelse.length > antTegnSomAlltidVises && (
-                    <Lenkeknapp onClick={() => setSkalViseHele(!skalViseHeleBeskrivelse)}>
-                        {skalViseHeleBeskrivelse ? 'Skjul beskrivelse' : 'Les mer'}
-                        <NavFrontendChevron type={skalViseHeleBeskrivelse ? 'opp' : 'ned'} />
-                    </Lenkeknapp>
+                    <span>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <Lenkeknapp onClick={() => setSkalViseHele(!skalViseHeleBeskrivelse)}>
+                            {skalViseHeleBeskrivelse ? 'Skjul beskrivelse' : 'Les mer'}
+                            <NavFrontendChevron type={skalViseHeleBeskrivelse ? 'opp' : 'ned'} />
+                        </Lenkeknapp>
+                    </span>
                 )}
             </Normaltekst>
         </>
