9c9ceb45a8005c2080fe23d4a49e10277343c75e
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-05-29 08:03:22 +0200

    Juster grid-layout på små skjermer

--- src/veileder/kandidatlister/KandidatlisterFilter/KandidatlisterFilter.tsx
+++ src/veileder/kandidatlister/KandidatlisterFilter/KandidatlisterFilter.tsx
@@ -19,49 +19,51 @@ export const KandidatlisterFilter: FunctionComponent<Props> = ({
 }) => (
     <div className="kandidatlister-filter">
         <Ekspanderbartpanel apen tittel={<Undertittel>Kandidatlister</Undertittel>}>
-            <SkjemaGruppe className="kandidatlister-filter__skjemagruppe">
-                <Radio
-                    label="Vis kun mine"
-                    name="kandidatliste-filter-eierskap"
-                    checked={kandidatlisterSokeKriterier.kunEgne}
-                    onChange={() => onVisMineKandidatlister()}
-                />
-                <Radio
-                    label="Vis alle sine"
-                    name="kandidatliste-filter-eierskap"
-                    checked={!kandidatlisterSokeKriterier.kunEgne}
-                    onChange={() => onVisAlleKandidatlister()}
-                />
-            </SkjemaGruppe>
-            <SkjemaGruppe className="kandidatlister-filter__skjemagruppe">
-                <Radio
-                    id="alle-kandidatlister-radio"
-                    label="Alle kandidatlister"
-                    name="kandidatlister-filter-stilling"
-                    className="kandidatlister-filter__radio"
-                    value=""
-                    checked={kandidatlisterSokeKriterier.type === ''}
-                    onChange={onFilterChange}
-                />
-                <Radio
-                    id="kandidatlister-til-stilling-radio"
-                    label="Kandidatlister knyttet til stilling"
-                    name="kandidatlister-filter-stilling"
-                    className="kandidatlister-filter__radio"
-                    value="MED_STILLING"
-                    checked={kandidatlisterSokeKriterier.type === 'MED_STILLING'}
-                    onChange={onFilterChange}
-                />
-                <Radio
-                    id="kandidatlister-uten-stilling-radio"
-                    label="Kandidatlister uten stilling"
-                    name="kandidatlister-filter-stilling"
-                    className="kandidatlister-filter__radio"
-                    value="UTEN_STILLING"
-                    checked={kandidatlisterSokeKriterier.type === 'UTEN_STILLING'}
-                    onChange={onFilterChange}
-                />
-            </SkjemaGruppe>
+            <div className="kandidatlister-filter__innhold">
+                <SkjemaGruppe className="kandidatlister-filter__skjemagruppe">
+                    <Radio
+                        label="Vis kun mine"
+                        name="kandidatliste-filter-eierskap"
+                        checked={kandidatlisterSokeKriterier.kunEgne}
+                        onChange={() => onVisMineKandidatlister()}
+                    />
+                    <Radio
+                        label="Vis alle sine"
+                        name="kandidatliste-filter-eierskap"
+                        checked={!kandidatlisterSokeKriterier.kunEgne}
+                        onChange={() => onVisAlleKandidatlister()}
+                    />
+                </SkjemaGruppe>
+                <SkjemaGruppe className="kandidatlister-filter__skjemagruppe">
+                    <Radio
+                        id="alle-kandidatlister-radio"
+                        label="Alle kandidatlister"
+                        name="kandidatlister-filter-stilling"
+                        className="kandidatlister-filter__radio"
+                        value=""
+                        checked={kandidatlisterSokeKriterier.type === ''}
+                        onChange={onFilterChange}
+                    />
+                    <Radio
+                        id="kandidatlister-til-stilling-radio"
+                        label="Kandidatlister knyttet til stilling"
+                        name="kandidatlister-filter-stilling"
+                        className="kandidatlister-filter__radio"
+                        value="MED_STILLING"
+                        checked={kandidatlisterSokeKriterier.type === 'MED_STILLING'}
+                        onChange={onFilterChange}
+                    />
+                    <Radio
+                        id="kandidatlister-uten-stilling-radio"
+                        label="Kandidatlister uten stilling"
+                        name="kandidatlister-filter-stilling"
+                        className="kandidatlister-filter__radio"
+                        value="UTEN_STILLING"
+                        checked={kandidatlisterSokeKriterier.type === 'UTEN_STILLING'}
+                        onChange={onFilterChange}
+                    />
+                </SkjemaGruppe>
+            </div>
         </Ekspanderbartpanel>
     </div>
 );
