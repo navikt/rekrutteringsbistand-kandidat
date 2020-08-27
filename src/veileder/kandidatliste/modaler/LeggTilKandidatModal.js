563356d60ebccd598fd87df0dde0302bd6e08fd5
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-08-27 15:21:34 +0200

    Fjern bold, endre tekst i legg-til-kandidat-modal

--- src/veileder/kandidatliste/modaler/LeggTilKandidatModal.js
+++ src/veileder/kandidatliste/modaler/LeggTilKandidatModal.js
@@ -124,7 +124,10 @@ class LeggTilKandidatModal extends React.Component {
 
     kandidatenFinnesIkke = () => (
         <div className="skjemaelement__feilmelding">
-            <div className="blokk-xxs">Du kan ikke legge til kandidaten.</div>
+            <div className="blokk-xxs">
+                Du kan ikke legge til kandidaten, fordi personen ikke er synlig i
+                Rekrutteringsbistand.
+            </div>
             <div>Mulige årsaker:</div>
             <ul className="leggTilKandidatModal--feilmelding__ul">
                 <li>Fødselsnummeret er feil</li>
@@ -132,9 +135,9 @@ class LeggTilKandidatModal extends React.Component {
                 <li>Kandidaten har ikke CV</li>
                 <li>Kandidaten har ikke lest hjemmel i ny CV-løsning</li>
                 <li>Kandidaten er egen ansatt, og du har ikke tilgang</li>
-                <li>{'Kandidaten har "Nei nav.no" i Formidlingsinformasjon i Arena'}</li>
-                <li>{'Kandidaten har personforhold "Fritatt for kandidatsøk" i Arena'}</li>
-                <li>{'Kandidaten er sperret "Egen ansatt"'}</li>
+                <li>Kandidaten har "Nei nav.no" i Formidlingsinformasjon i Arena</li>
+                <li>Kandidaten har personforhold "Fritatt for kandidatsøk" i Arena</li>
+                <li>Kandidaten er sperret "Egen ansatt"</li>
             </ul>
         </div>
     );
