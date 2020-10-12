321ca167c8f3e3e51f64f59b20f5b58ee6df5c1f
Joar Aurdal <joar.aurdal@nav.no>
2020-10-12 16:31:33 +0200

    jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     transform: {
-        "^.+\\.(css|less)$": "./styleMock.js"
+        "\\.(css|less)$": "./styleMock.js",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "./fileMock.js",
       }
 };
