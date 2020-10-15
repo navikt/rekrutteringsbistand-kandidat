e2adfbdf1ee94bc2aa143bf134ed2a61318a99f1
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-15 12:14:25 +0200

    Bruk en server-fil tilsvarende i miljø

---
+++ labs/server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const server = express();
+
+const port = process.env.PORT || 8080;
+server.set('port', port);
+
+const startServer = () => {
+    server.get('/rekrutteringsbistand-kandidat/internal/isAlive', (req, res) =>
+        res.sendStatus(200)
+    );
+    server.get('/rekrutteringsbistand-kandidat/internal/isReady', (req, res) =>
+        res.sendStatus(200)
+    );
+
+    server.use('/kandidater/js', express.static(path.resolve(__dirname, '../dist/js')));
+    server.use('/kandidater/css', express.static(path.resolve(__dirname, '../dist/css')));
+
+    server.use(
+        ['/kandidater', '/kandidater/*'],
+        express.static(path.resolve(__dirname, 'index.html'))
+    );
+
+    server.listen(port, () => {
+        console.log(`Express-server startet. Server filer fra ./dist/ til localhost:${port}/`);
+        console.log(`Versjon: ${process.env.APP_VERSION}`);
+    });
+};
+
+startServer();
