36da510e164e91cb2ff349db20afb24b44795955
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-11-19 11:33:51 +0100

    Sett alle feature-toggles defaults til 'false' slik det er i miljø

--- src/veileder/mock/data/feature-toggles.mock.ts
+++ src/veileder/mock/data/feature-toggles.mock.ts
@@ -1,6 +1,6 @@
 import FEATURE_TOGGLES from '../../../felles/konstanter';
 
 export const featureToggles = FEATURE_TOGGLES.reduce(
-    (alleToggles, toggle) => ({ ...alleToggles, [toggle]: true }),
+    (alleToggles, toggle) => ({ ...alleToggles, [toggle]: false }),
     {}
 );
