55d8253197d1829a795b6566bb5b799227b9d634
Joar Aurdal <joar.aurdal@gmail.com>
2020-11-27 13:56:03 +0100

    labs sammen med ny microfrontend (#263)
    * labs sammen med ny microfrontend
    
    * labs sammen med ny microfrontend
    
    * Ta bort prefix for labs
    
    * Flytt miljøvariable til package.json
    
    * Flytt eksport config til package.json
    
    * deploy labs nytt navn
    
    * Fiks url til liveness for labs
    
    * Fiks isAlive
    
    * Endre server image
    
    * Endre path docker labs
    
    * Endre path docker labs
    
    * Endre labs buildpath
    
    * Fiks path ccs og js for labs
    
    * Fiks path css og js for labs
    
    * Fiks path css og js for labs
    
    * Fiks path css og js for labs
    
    * Fiks path css og js for labs
    
    * Ta bort appPrefix i url
    
    * Legg på basepath for labs

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { AppProps, Main } from './veileder/App';
 import eksporterApp from './eksporterApp';
 import Utviklingsapp from './utviklingsapp/Utviklingsapp';
 
-const skalEksporteres = process.env.REACT_APP_EXPORT || process.env.NODE_ENV === 'production';
-
 // Alle klassenavn blir prefikset med ".rek-kandidat" i craco-configen, så også koden
 // som brukes under utvikling må wrappes i et element med dette klassenavnet.
 export const cssScopeForApp = 'rek-kandidat';
@@ -16,7 +14,7 @@ const AppMedCssScope: FunctionComponent<AppProps> = (props) => (
     </div>
 );
 
-if (skalEksporteres) {
+if (process.env.REACT_APP_EXPORT) {
     eksporterApp('rekrutteringsbistand-kandidat', AppMedCssScope);
 } else {
     ReactDOM.render(<Utviklingsapp />, document.getElementById('utviklingsapp'));
