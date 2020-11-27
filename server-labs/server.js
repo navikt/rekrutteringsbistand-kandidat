94385faa604b0470b4ae55bcb5af51bb9a32311e
Joar Aurdal <joar.aurdal@nav.no>
2020-11-27 08:54:33 +0100

    Fiks path css og js for labs

--- server-labs/server.js
+++ server-labs/server.js
@@ -3,17 +3,21 @@ const path = require('path');
 
 const server = express();
 
-const prefix = ``;
-
 const startServer = () => {
-    server.get(`${prefix}/internal/isAlive`, (_, res) => res.sendStatus(200));
-    server.get(`${prefix}/internal/isReady`, (_, res) => res.sendStatus(200));
+    server.get(`/internal/isAlive`, (_, res) => res.sendStatus(200));
+    server.get(`/internal/isReady`, (_, res) => res.sendStatus(200));
 
     const buildPath = path.join(__dirname, 'build');
 
-    server.use(`${prefix}/js`, express.static(`${buildPath}/static/js`));
-    server.use(`${prefix}/css`, express.static(`${buildPath}/static/css`));
-    server.get([`${prefix}/kandidater`, `${prefix}/kandidater/*`], (_, res) => {
+    server.use(
+        `/rekrutteringsbistand-kandidat/static/js`,
+        express.static(`${buildPath}/static/js`)
+    );
+    server.use(
+        `/rekrutteringsbistand-kandidat/static/css`,
+        express.static(`${buildPath}/static/css`)
+    );
+    server.get([`/kandidater`, `${prefix}/kandidater/*`], (_, res) => {
         res.sendFile(`${buildPath}/index.html`);
     });
 
