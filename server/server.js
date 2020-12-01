d3f508fb73c2e575da24089c2458fca1ee7b9b6b
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-12-01 09:01:32 +0100

    Fjern rester fra gammel proxy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxy = require('express-http-proxy');
+const fjernDobleCookies = require('./cookies');
 
 const fs = require('fs');
 const app = express();
@@ -26,65 +26,6 @@ const writeEnvironmentVariablesToFile = () => {
     });
 };
 
-const mapToCookies = (cookieString) =>
-    cookieString
-        .split(';')
-        .filter((str) => !!str && str !== '')
-        .map((cookieStr) => ({
-            name: cookieStr.split('=')[0].trim(),
-            value: cookieStr.split('=')[1].trim(),
-        }));
-
-const mapToCookieString = (cookieList) =>
-    cookieList.map((cookie) => `${cookie.name}=${cookie.value}`).join(';');
-
-const lagCookieStringUtenDobleCookies = (cookieString) => {
-    const cookies = mapToCookies(cookieString);
-
-    const cookiesUtenDuplikater = cookies.filter((cookie) => {
-        const duplicates = cookies.filter((cookie2) => cookie2.name === cookie.name);
-        if (duplicates.length === 1) {
-            return true;
-        }
-        return cookie.value === duplicates[0].value;
-    });
-
-    const forskjellIAntallCookies = cookies.length - cookiesUtenDuplikater.length;
-    if (forskjellIAntallCookies !== 0) {
-        console.log('Fjernet ' + forskjellIAntallCookies + ' cookies');
-    }
-
-    return mapToCookieString(cookiesUtenDuplikater);
-};
-
-const fjernDobleCookies = (req, res, next) => {
-    req.headers.cookie = lagCookieStringUtenDobleCookies(req.headers.cookie);
-    next();
-};
-
-const konfigurerProxyTilMidlertidigUtilgjengeligApi = () => {
-    const [, , host, ...pathParts] = process.env.MIDLERTIDIG_UTILGJENGELIG_API.split('/');
-    const path = pathParts.join('/');
-
-    app.use(`${basePath}/midlertidig-utilgjengelig-api`, [
-        fjernDobleCookies,
-        proxy(host, {
-            https: true,
-            proxyReqPathResolver: (request) => {
-                const nyPath = request.originalUrl.replace(
-                    new RegExp(`${basePath}/midlertidig-utilgjengelig-api`),
-                    path
-                );
-
-                console.log(
-                    `~> Proxy midl.util. fra path '${request.originalUrl}' til URL '${host}/${nyPath}`
-                );
-                return nyPath;
-            },
-        }),
-    ]);
-};
-
 const setupProxy = (fraPath, tilTarget, headers = undefined) =>
     createProxyMiddleware(fraPath, {
         target: tilTarget,
@@ -103,6 +44,7 @@ const startServer = () => {
     writeEnvironmentVariablesToFile();
 
     app.use(setupProxy(`${basePath}/kandidat-api`, process.env.KANDIDATSOK_API_URL));
+
     app.use(
         setupProxy(`${basePath}/enhetsregister-api`, process.env.ENHETSREGISTER_API, {
             'x-nav-apiKey': miljøvariablerFraVault.ENHETSREGISTER_GATEWAY_APIKEY,
@@ -110,6 +52,7 @@ const startServer = () => {
     );
 
     app.use(setupProxy(`${basePath}/sms-api`, process.env.SMS_API));
+
     app.use(`${basePath}/midlertidig-utilgjengelig-api`, [
         fjernDobleCookies,
         setupProxy(
@@ -117,8 +60,6 @@ const startServer = () => {
             process.env.MIDLERTIDIG_UTILGJENGELIG_API
         ),
     ]);
-
-    // konfigurerProxyTilMidlertidigUtilgjengeligApi();
 
     app.use(`${basePath}/static`, express.static(buildPath + '/static'));
     app.use(`${basePath}/asset-manifest.json`, express.static(`${buildPath}/asset-manifest.json`));
