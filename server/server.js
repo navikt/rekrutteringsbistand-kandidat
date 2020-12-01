49bb98b6cf67019c9f384a3c5ea034dcb70486df
Joar Aurdal <joar.aurdal@nav.no>
2020-12-01 16:23:25 +0100

    Legg til env i manifest

--- server/server.js
+++ server/server.js
@@ -45,10 +45,13 @@ const startServer = () => {
 
     app.use(`${basePath}/static`, express.static(buildPath + '/static'));
 
-    fs.readFile(`${buildPath}/asset-manifest.json`, 'utf8', function (err, data) {
-        data.files['env.js'] = '/rekrutteringsbistand-kandidat/static/js/env.js';
-        app.get(`${basePath}/asset-manifest.json`, data);
-    });
+    const asset = fs.readFileSync(`${buildPath}/asset-manifest.json`, 'utf8');
+    console.log('asset original', asset);
+    if (asset.files) {
+        asset.files['env.js'] = '/rekrutteringsbistand-kandidat/static/js/env.js';
+    }
+    console.log('asset endret', asset);
+    app.get(`${basePath}/asset-manifest.json`, asset);
 
     app.get([`${basePath}/internal/isAlive`, `${basePath}/internal/isReady`], (req, res) =>
         res.sendStatus(200)
