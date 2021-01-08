dadc13716515e4e3ccbbf999f717f262f59c2fc7
Joar Aurdal <joar.aurdal@gmail.com>
2021-01-08 08:38:07 +0100

    Endre legg til kandidat feilmelding (#278)

--- src/veileder/kandidatliste/modaler/legg-til-kandidat-modal/KandidatenFinnesIkke.tsx
+++ src/veileder/kandidatliste/modaler/legg-til-kandidat-modal/KandidatenFinnesIkke.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 
 const KandidatenFinnesIkke = () => (
     <div className="skjemaelement__feilmelding">
-        <div className="blokk-xxs">
-            Du kan ikke legge til kandidaten, fordi personen ikke er synlig i Rekrutteringsbistand.
-        </div>
+        <div className="blokk-xxs">Kandidaten kan ikke legges til.</div>
         <div>Mulige årsaker:</div>
         <ul className="leggTilKandidatModal--feilmelding__ul">
             <li>Fødselsnummeret er feil</li>
-            <li>Kandidaten har ikke jobbprofil</li>
-            <li>Kandidaten har ikke CV</li>
-            <li>Kandidaten har ikke lest hjemmel i ny CV-løsning</li>
-            <li>Kandidaten er egen ansatt, og du har ikke tilgang</li>
-            <li>Kandidaten har "Nei nav.no" i Formidlingsinformasjon i Arena</li>
+            <li>Kandidaten mangler CV eller jobbprofil</li>
+            <li>Kandidaten har ikke blitt informert om NAVs behandlingsgrunnlag</li>
             <li>Kandidaten har personforhold "Fritatt for kandidatsøk" i Arena</li>
             <li>Kandidaten er sperret "Egen ansatt"</li>
+            <li>Kandidaten har diskresjonskode (kode 6 og 7)</li>
         </ul>
     </div>
 );
