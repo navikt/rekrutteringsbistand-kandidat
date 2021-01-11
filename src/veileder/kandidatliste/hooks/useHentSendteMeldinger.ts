cdfa8842ea5cdb9e52df3074387c21a53e053872
André Mossige <mossige.a@gmail.com>
2021-01-11 13:40:33 +0100

    Flytt henting av SMS til et sted hvor vi alltid har kandidatliste

--- src/veileder/kandidatliste/hooks/useHentSendteMeldinger.ts
+++ src/veileder/kandidatliste/hooks/useHentSendteMeldinger.ts
@@ -1,27 +1,15 @@
 import { useDispatch } from 'react-redux';
-import { Sms } from '../kandidatlistetyper';
 import { useEffect } from 'react';
-import { RemoteData, Nettstatus } from '../../../felles/common/remoteData';
-import { Kandidatliste } from '../kandidatlistetyper';
 import KandidatlisteActionType from '../reducer/KandidatlisteActionType';
 
-const useHentSendteMeldinger = (
-    kandidatliste: RemoteData<Kandidatliste>,
-    kandidatmeldinger: RemoteData<Sms[]>
-) => {
+const useHentSendteMeldinger = (kandidatlisteId: string) => {
     const dispatch = useDispatch();
-
     useEffect(() => {
-        if (
-            kandidatliste.kind === Nettstatus.Suksess &&
-            kandidatmeldinger.kind === Nettstatus.IkkeLastet
-        ) {
-            dispatch({
-                type: KandidatlisteActionType.HENT_SENDTE_MELDINGER,
-                kandidatlisteId: kandidatliste.data.kandidatlisteId,
-            });
-        }
-    }, [dispatch, kandidatliste, kandidatmeldinger]);
+        dispatch({
+            type: KandidatlisteActionType.HENT_SENDTE_MELDINGER,
+            kandidatlisteId: kandidatlisteId,
+        });
+    }, [kandidatlisteId]);
 };
 
 export default useHentSendteMeldinger;
