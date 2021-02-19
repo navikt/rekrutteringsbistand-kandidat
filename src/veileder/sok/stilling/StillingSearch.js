bbcf44f66e9296e16b1a8f54708663f9f9c9eebe
Mads Lee Giil <mads.lee.giil@nav.no>
2021-02-19 10:31:40 +0100

    Flytt innhold fra felles Stilling-filter til kandidatsøkets Stilling-filter

--- src/veileder/sok/stilling/StillingSearch.js
+++ src/veileder/sok/stilling/StillingSearch.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import StillingSearchFelles from '../../../felles/sok/stilling/StillingSearch';
@@ -13,6 +13,12 @@ import {
     FETCH_TYPE_AHEAD_SUGGESTIONS,
 } from '../../common/typeahead/typeaheadReducer';
 import { ALERTTYPE, BRANCHNAVN } from '../../../felles/konstanter';
+import SokekriteriePanel from '../../../felles/common/sokekriteriePanel/SokekriteriePanel';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
+import Typeahead from '../typeahead/Typeahead';
+import { Knapp } from 'nav-frontend-knapper';
+import { Merkelapp } from 'pam-frontend-merkelapper';
+import AlertStripeInfo from '../../../felles/common/AlertStripeInfo';
 
 const StillingSearch = ({ ...props }) => {
     const {
@@ -29,30 +35,123 @@ const StillingSearch = ({ ...props }) => {
         fetchKompetanseSuggestions,
         togglePanelOpen,
         stillingsId,
+        useJanzz,
+        allowOnlyTypeaheadSuggestions,
     } = props;
+
+    const [showTypeAhead, setShowTypeAhead] = useState(false);
+    const [typeAheadValue, setTypeAheadValue] = useState('');
+    const typeAhead = useRef(null);
+
+    const onTypeAheadStillingChange = (value) => {
+        fetchTypeAheadSuggestions(value);
+        setTypeAheadValue(value);
+    };
+
+    const onTypeAheadStillingSelect = (value) => {
+        if (value !== '') {
+            selectTypeAheadValue(value);
+            clearTypeAheadStilling();
+            setTypeAheadValue('');
+            fetchKompetanseSuggestions();
+            search();
+        }
+    };
+
+    const onLeggTilClick = () => {
+        setShowTypeAhead(true);
+        typeAhead.current?.input.focus();
+    };
+
+    const onFjernClick = (stilling) => {
+        removeStilling(stilling);
+        fetchKompetanseSuggestions();
+        search();
+    };
+
+    const onTypeAheadBlur = () => {
+        setTypeAheadValue('');
+        setShowTypeAhead(false);
+        clearTypeAheadStilling();
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        this.onTypeAheadStillingSelect(typeAheadValue);
+        this.typeAhead.input.focus();
+    };
+
     return (
-        <StillingSearchFelles
-            stillinger={stillinger}
-            typeAheadSuggestionsStilling={typeAheadSuggestionsStilling}
-            totaltAntallTreff={totaltAntallTreff}
-            visAlertFaKandidater={visAlertFaKandidater}
-            panelOpen={panelOpen}
-            search={search}
-            clearTypeAheadStilling={clearTypeAheadStilling}
-            fetchTypeAheadSuggestions={fetchTypeAheadSuggestions}
-            selectTypeAheadValue={selectTypeAheadValue}
-            removeStilling={removeStilling}
-            fetchKompetanseSuggestions={fetchKompetanseSuggestions}
-            togglePanelOpen={togglePanelOpen}
-            stillingsId={stillingsId}
-            allowOnlyTypeaheadSuggestions
-        />
+        <SokekriteriePanel
+            id="Stilling__SokekriteriePanel"
+            fane="stilling-yrke"
+            tittel="Stilling/yrke"
+            onClick={togglePanelOpen}
+            apen={panelOpen === undefined && stillingsId ? true : panelOpen}
+        >
+            <Element>Hva slags kandidat trenger du?</Element>
+            <Normaltekst>For eksempel: pedagogisk leder</Normaltekst>
+            <div className="sokekriterier--kriterier">
+                {/* TODO: Fjerne feature toggle */}
+                {!(useJanzz && stillinger.length > 0) && (
+                    <div>
+                        {showTypeAhead ? (
+                            <Typeahead
+                                ref={(typeAheadRef) => {
+                                    typeAhead.current = typeAheadRef;
+                                }}
+                                onSelect={onTypeAheadStillingSelect}
+                                onChange={onTypeAheadStillingChange}
+                                label=""
+                                name="stilling"
+                                placeholder="Skriv inn stilling/yrke"
+                                suggestions={typeAheadSuggestionsStilling}
+                                value={typeAheadValue}
+                                id="typeahead-stilling"
+                                onSubmit={onSubmit}
+                                onTypeAheadBlur={onTypeAheadBlur}
+                                allowOnlyTypeaheadSuggestions={allowOnlyTypeaheadSuggestions}
+                                selectedSuggestions={stillinger}
+                            />
+                        ) : (
+                            <Knapp
+                                onClick={onLeggTilClick}
+                                id="leggtil-stilling-knapp"
+                                className="knapp-små-bokstaver"
+                                kompakt
+                            >
+                                + Legg til stilling/yrke
+                            </Knapp>
+                        )}
+                    </div>
+                )}
+                <div className="Merkelapp__wrapper">
+                    {stillinger.map((stilling) => (
+                        <Merkelapp onRemove={onFjernClick} key={stilling} value={stilling}>
+                            {stilling}
+                        </Merkelapp>
+                    ))}
+                </div>
+            </div>
+
+            {/* TODO: Fjerne feature toggle */}
+            {useJanzz && (
+                <Normaltekst className="blokk-xs">
+                    Du kan kun legge til én stilling/yrke
+                </Normaltekst>
+            )}
+            {totaltAntallTreff <= 10 && visAlertFaKandidater === ALERTTYPE.STILLING && (
+                <AlertStripeInfo totaltAntallTreff={totaltAntallTreff} />
+            )}
+        </SokekriteriePanel>
     );
 };
 
 StillingSearch.defaultProps = {
     panelOpen: undefined,
     stillingsId: undefined,
+    useJanzz: false,
+    allowOnlyTypeaheadSuggestions: false,
 };
 
 StillingSearch.propTypes = {
@@ -69,6 +168,8 @@ StillingSearch.propTypes = {
     panelOpen: PropTypes.bool,
     togglePanelOpen: PropTypes.func.isRequired,
     stillingsId: PropTypes.string,
+    useJanzz: PropTypes.bool,
+    allowOnlyTypeaheadSuggestions: PropTypes.bool,
 };
 
 const mapStateToProps = (state) => ({
