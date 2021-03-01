a10c038247078bb0788fc84964831f09fcd3cc03
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-03-01 10:00:23 +0100

    Fjern connect i KandidatsøkUtenKontekst, bruk hooks

--- src/kandidatsøk/KandidatsøkUtenKontekst.tsx
+++ src/kandidatsøk/KandidatsøkUtenKontekst.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { FellesKandidatsøkProps } from './FellesKandidatsøk';
+import { useDispatch, useSelector } from 'react-redux';
 import { harUrlParametere } from './reducer/searchQuery';
 import { Kandidatsøk } from './Kandidatsøk';
 import { SEARCH, SØK_MED_URL_PARAMETERE } from './reducer/searchReducer';
@@ -8,37 +7,31 @@ import AppState from '../AppState';
 import useNullstillKandidatlisteState from './useNullstillKandidatlistestate';
 import './Resultat.less';
 
-type Props = FellesKandidatsøkProps & {
-    søkestateKommerFraAnnetSøk: boolean;
-    nullstillKandidaterErLagretIKandidatlisteAlert: () => void;
-};
+const KandidatsøkUtenKontekst: FunctionComponent = () => {
+    const dispatch = useDispatch();
+    const søkestateKommerFraAnnetSøk = useSelector(
+        (state: AppState) => !!state.søk.kandidatlisteId
+    );
 
-const KandidatsøkUtenKontekst: FunctionComponent<Props> = ({
-    search,
-    leggUrlParametereIStateOgSøk,
-    søkestateKommerFraAnnetSøk,
-}) => {
     useNullstillKandidatlisteState();
 
     useEffect(() => {
+        const leggUrlParametereIStateOgSøk = (href: string) => {
+            dispatch({ type: SØK_MED_URL_PARAMETERE, href });
+        };
+
+        const søk = () => {
+            dispatch({ type: SEARCH });
+        };
+
         if (søkestateKommerFraAnnetSøk || harUrlParametere(window.location.href)) {
             leggUrlParametereIStateOgSøk(window.location.href);
         } else {
-            search();
+            søk();
         }
-    }, [leggUrlParametereIStateOgSøk, søkestateKommerFraAnnetSøk, search]);
+    }, [dispatch, søkestateKommerFraAnnetSøk]);
 
     return <Kandidatsøk />;
 };
 
-const mapStateToProps = (state: AppState) => ({
-    søkestateKommerFraAnnetSøk: !!state.søk.kandidatlisteId,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-    search: () => dispatch({ type: SEARCH }),
-    leggUrlParametereIStateOgSøk: (href: string) =>
-        dispatch({ type: SØK_MED_URL_PARAMETERE, href }),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(KandidatsøkUtenKontekst);
+export default KandidatsøkUtenKontekst;
