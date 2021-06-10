4455e9c39981c347baf15e9e87c941a0e0aa7d31
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-06-10 12:46:19 +0200

    Vis utfall som etikett på kandidaten

--- src/kandidatliste/kandidatrad/status-og-hendelser/StatusOgHendelser.tsx
+++ src/kandidatliste/kandidatrad/status-og-hendelser/StatusOgHendelser.tsx
@@ -3,20 +3,38 @@ import Etikett from 'nav-frontend-etiketter';
 import { KandidatIKandidatliste } from '../../kandidatlistetyper';
 import { statusToDisplayName } from '../statusSelect/StatusSelect';
 import './StatusOgHendelser.less';
+import { Utfall } from '../utfall-med-endre-ikon/UtfallMedEndreIkon';
 
 type Props = {
     kandidat: KandidatIKandidatliste;
 };
 
 const StatusOgHendelser: FunctionComponent<Props> = ({ kandidat }) => {
-    let etikettClassName = 'status-og-hendelser__status';
-    etikettClassName += ` status-og-hendelser__status--${kandidat.status.toLowerCase()}`;
+    const etikettClassName = `status-og-hendelser__status status-og-hendelser__status--${kandidat.status.toLowerCase()}`;
 
     return (
         <div className="status-og-hendelser">
             <Etikett mini type="info" className={etikettClassName}>
                 {statusToDisplayName(kandidat.status)}
             </Etikett>
+            {kandidat.utfall === Utfall.Presentert && (
+                <Etikett
+                    mini
+                    type="info"
+                    className="status-og-hendelser__hendelse status-og-hendelser__hendelse--presentert"
+                >
+                    CV delt
+                </Etikett>
+            )}
+            {kandidat.utfall === Utfall.FåttJobben && (
+                <Etikett
+                    mini
+                    type="info"
+                    className="status-og-hendelser__hendelse status-og-hendelser__hendelse--fatt-jobben"
+                >
+                    Fått jobben
+                </Etikett>
+            )}
         </div>
     );
 };
