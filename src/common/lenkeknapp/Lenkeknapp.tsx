d6c73d9190024e60402104f22adcfc9b699644a6
André Mossige <andre.mossige@nav.no>
2021-07-01 13:11:09 +0200

    Skal ikke kunne disable lenkeknapp likevel

--- src/common/lenkeknapp/Lenkeknapp.tsx
+++ src/common/lenkeknapp/Lenkeknapp.tsx
@@ -6,19 +6,11 @@ type Props = {
     onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
     className?: string;
     tittel?: string;
-    disabled?: boolean;
 };
 
-const Lenkeknapp: FunctionComponent<Props> = ({
-    children,
-    onClick,
-    className = '',
-    tittel,
-    disabled,
-}) => {
+const Lenkeknapp: FunctionComponent<Props> = ({ children, onClick, className = '', tittel }) => {
     return (
         <button
-            disabled={disabled}
             className={classNames('Lenkeknapp', 'typo-normal', className)}
             onClick={onClick}
             title={tittel}
