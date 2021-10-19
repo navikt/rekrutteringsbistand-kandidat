e4f6fad5d9e8bfbeedd03479dfc4e825525a3dbf
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2021-10-19 12:06:47 +0200

    Lanser flyt med godkjenning i aktivitetsplan (#351)

--- src/kandidatliste/hooks/useHentForespørslerOmDelingAvCv.ts
+++ src/kandidatliste/hooks/useHentForespørslerOmDelingAvCv.ts
@@ -2,13 +2,12 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import KandidatlisteActionType from '../reducer/KandidatlisteActionType';
 import KandidatlisteAction from '../reducer/KandidatlisteAction';
-import { erIkkeProd } from '../../utils/featureToggleUtils';
 
 const useHentForespørslerOmDelingAvCv = (stillingsId: string | null) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if (stillingsId && erIkkeProd) {
+        if (stillingsId) {
             dispatch<KandidatlisteAction>({
                 type: KandidatlisteActionType.HentForespørslerOmDelingAvCv,
                 stillingsId,
