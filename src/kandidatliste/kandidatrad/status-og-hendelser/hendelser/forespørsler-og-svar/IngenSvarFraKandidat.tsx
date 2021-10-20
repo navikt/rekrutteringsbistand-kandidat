6abbc84122911d293b11a71ba47a20afa0d1a343
Mads Lee Giil <mads.lee.giil@nav.no>
2021-10-20 08:15:36 +0200

    Vis dato for frist utløpt når denne er mer enn 3 dager tilbake i tid

--- src/kandidatliste/kandidatrad/status-og-hendelser/hendelser/forespørsler-og-svar/IngenSvarFraKandidat.tsx
+++ src/kandidatliste/kandidatrad/status-og-hendelser/hendelser/forespørsler-og-svar/IngenSvarFraKandidat.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import moment from 'moment';
 import { TilstandPåForespørsel } from '../../../../knappe-rad/forespørsel-om-deling-av-cv/Forespørsel';
 import Hendelse, { Hendelsesstatus } from '../Hendelse';
+import { formaterDatoUtenÅrstall } from '../../../../../utils/dateUtils';
 
 type Props = {
     tilstand: TilstandPåForespørsel;
@@ -9,7 +10,6 @@ type Props = {
 };
 
 const IngenSvarFraKandidat: FunctionComponent<Props> = ({ svarfrist, tilstand, children }) => {
-    // TODO: Fjern de tre ekstra timene når vi får riktig tidssone fra backend
     const dagerTilSvarfristDesimal = moment(svarfrist).diff(moment(), 'days', true);
     const forespørselErUtløpt =
         dagerTilSvarfristDesimal < 0 || tilstand === TilstandPåForespørsel.Avbrutt;
@@ -18,20 +18,28 @@ const IngenSvarFraKandidat: FunctionComponent<Props> = ({ svarfrist, tilstand, c
         <Hendelse
             status={forespørselErUtløpt ? Hendelsesstatus.Blå : Hendelsesstatus.Hvit}
             tittel="Svar fra kandidat om deling av CV"
-            beskrivelse={formaterSvarfrist(dagerTilSvarfristDesimal, tilstand)}
+            beskrivelse={formaterSvarfrist(dagerTilSvarfristDesimal, svarfrist, tilstand)}
         >
             {children}
         </Hendelse>
     );
 };
 
-const formaterSvarfrist = (dagerTilSvarfristDesimal: number, tilstand: TilstandPåForespørsel) => {
+const formaterSvarfrist = (
+    dagerTilSvarfristDesimal: number,
+    svarfrist: string,
+    tilstand: TilstandPåForespørsel
+) => {
     const dagerTilSvarfrist = Math.floor(dagerTilSvarfristDesimal);
 
     if (dagerTilSvarfristDesimal < 0) {
-        return dagerTilSvarfrist === -1
-            ? 'Svarfristen utløp i går'
-            : `Svarfristen utløp for ${dagerTilSvarfrist * -1} dager siden`;
+        if (dagerTilSvarfrist === -1) {
+            return 'Svarfristen utløp i går';
+        } else {
+            return dagerTilSvarfrist < -3
+                ? `Svarfristen utløp ${formaterDatoUtenÅrstall(svarfrist)}`
+                : `Svarfristen utløp for ${dagerTilSvarfrist * -1} dager siden`;
+        }
     } else {
         if (tilstand === TilstandPåForespørsel.Avbrutt) {
             return 'Svarfristen er utløpt';
