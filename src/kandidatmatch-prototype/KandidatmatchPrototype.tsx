435c9740da01e683b9c49d26a8aff0041a3aa123
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-05-04 14:15:49 +0200

    Vis utdanning og arbeidserfaring

--- src/kandidatmatch-prototype/KandidatmatchPrototype.tsx
+++ src/kandidatmatch-prototype/KandidatmatchPrototype.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import Prototype from './Prototype';
+import './KandidatmatchPrototype.less';
 
 const KandidatmatchPrototype: FunctionComponent = () => {
     const [prototype, setPrototype] = useState<Prototype[] | undefined>(undefined);
@@ -25,10 +26,37 @@ const KandidatmatchPrototype: FunctionComponent = () => {
     const kandidat = prototype ? prototype[0] : undefined;
 
     return (
-        <>
+        <div className="prototype">
             <h1>Elitekandidater</h1>
-            {kandidat && <h2>{kandidat.fornavn}</h2>}
-        </>
+            {kandidat && (
+                <>
+                    <div>
+                        <h2>{kandidat.fornavn}</h2>
+                        <p aria-label="fødselsnummer">{kandidat.fodselsnummer}</p>
+                    </div>
+                    <div>
+                        <h3>Arbeidserfaring ({kandidat.score_arbeidserfaring})</h3>
+                        <ul>
+                            {kandidat.arbeidserfaring.map((arbeidserfaring, index) => (
+                                <li key={arbeidserfaring.janzzKonseptid}>
+                                    {arbeidserfaring.stillingstittel} (
+                                    {kandidat.match_forklaring.arbeidserfaring_forklaring[index]})
+                                </li>
+                            ))}
+                        </ul>
+                        <h3>Utdanning ({kandidat.score_utdannelse})</h3>
+                        <ul>
+                            {kandidat.utdannelse.map((utdannelse, index) => (
+                                <li key={utdannelse.beskrivelse}>
+                                    {utdannelse.laerested} (
+                                    {kandidat.match_forklaring.utdannelse_forklaring[index]})
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </>
+            )}
+        </div>
     );
 };
 
