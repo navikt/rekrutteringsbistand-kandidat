4d5dba9bd114603f1c6764e0a3af5922abc8b05a
Joar Aurdal <joar.aurdal@nav.no>
2022-06-10 14:32:49 +0200

    Legg til mellomsteg for matchdetaljer

--- src/automatisk-matching/matchforklaring/Matrise.tsx
+++ src/automatisk-matching/matchforklaring/Matrise.tsx
@@ -9,10 +9,11 @@ type Props = {
     erfaring: ErfaringPrototype;
     tittel: string;
     match?: number;
-    visForkortetMatrise?: boolean;
 };
 
-const Matrise = ({ erfaring, tittel, match, visForkortetMatrise }: Props) => {
+const Matrise = ({ erfaring, tittel, match }: Props) => {
+    const minimumTreffprosent: Number = 25;
+
     if (erfaring.ordScore.length === 0) {
         return <IngenData />;
     }
@@ -29,42 +30,52 @@ const Matrise = ({ erfaring, tittel, match, visForkortetMatrise }: Props) => {
                     {match !== undefined && <span>({tilProsent(match)})</span>} {tittel}
                 </h3>
             </summary>
-            <div className="blokk-m">
-                <h4>Hvor godt matcher hvert ord i stillingsannonsen med ord fra kandidaten?</h4>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Ord fra stilling</th>
-                            <th colSpan={alleOrdFraKandidat.length}>
-                                Ord fra kandidaten og relasjon (%)
-                            </th>
-                        </tr>
-                    </thead>
-                    <thead>
-                        <tr>
-                            <th />
-                            {alleOrdFraKandidat.map((ord) => (
-                                <th key={`th-${ord}`}>{ord}</th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {erfaring.ordScore.map(
-                            ([[, ordFraStilling], matchedeOrdFraKandidaten], i) => {
-                                return (
-                                    <tr key={ordFraStilling}>
-                                        <td>{ordFraStilling}</td>
-                                        {matchedeOrdFraKandidaten.map(([, ord, score]) => (
-                                            <td key={ord}>{tilProsentpoeng(score)}</td>
-                                        ))}
-                                    </tr>
-                                );
-                            }
-                        )}
-                    </tbody>
-                </table>
-            </div>
-            {visForkortetMatrise && <ForkortetMatrise erfaring={erfaring} />}
+
+            <ForkortetMatrise erfaring={erfaring} minimumTreffprosent={minimumTreffprosent} />
+
+            <details className="matchdetaljer-matrise">
+                <summary>
+                    <Expand className="matchdetaljer-seksjon__ikon--ned" />
+                    <Collapse className="matchdetaljer-seksjon__ikon--opp" />
+                    Matchdetaljer
+                </summary>
+                <div className="blokk-m">
+                    <h4>Hvor godt matcher hvert ord i stillingsannonsen med ord fra kandidaten?</h4>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Ord fra stilling</th>
+                                <th colSpan={alleOrdFraKandidat.length}>
+                                    Ord fra kandidaten og relasjon (%)
+                                </th>
+                            </tr>
+                        </thead>
+                        <thead>
+                            <tr>
+                                <th />
+                                {alleOrdFraKandidat.map((ord) => (
+                                    <th key={`th-${ord}`}>{ord}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {erfaring.ordScore.map(
+                                ([[, ordFraStilling], matchedeOrdFraKandidaten], i) => {
+                                    return (
+                                        <tr key={ordFraStilling}>
+                                            <td>{ordFraStilling}</td>
+                                            {matchedeOrdFraKandidaten.map(([, ord, score]) => (
+                                                <td key={ord}>{tilProsentpoeng(score)}</td>
+                                            ))}
+                                        </tr>
+                                    );
+                                }
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+                )
+            </details>
         </details>
     );
 };
