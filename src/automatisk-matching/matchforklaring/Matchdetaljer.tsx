95df2e79da1d16c48ab19e0d9b2de0add3e07790
Joar Aurdal <joar.aurdal@nav.no>
2022-06-13 12:33:24 +0200

    Legg til manglede fil

---
+++ src/automatisk-matching/matchforklaring/Matchdetaljer.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Collapse, Expand } from '@navikt/ds-icons';
+import { ErfaringPrototype } from '../Kandidatmatch';
+import { tilProsentpoeng } from '../formatering';
+
+const Matchdetaljer = ({ erfaring }: { erfaring: ErfaringPrototype }) => {
+    const [, matchedeOrdFraKandidat] = erfaring.ordScore[0];
+    const alleOrdFraKandidat = matchedeOrdFraKandidat.map(([, ord]) => ord);
+
+    return (
+        <details className="matchdetaljer-matrise">
+            <summary>
+                <Expand className="matchdetaljer-seksjon__ikon--ned" />
+                <Collapse className="matchdetaljer-seksjon__ikon--opp" />
+                Matchdetaljer
+            </summary>
+            <div className="blokk-m">
+                <h4>Hvor godt matcher hvert ord i stillingsannonsen med ord fra kandidaten?</h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Ord fra stilling</th>
+                            <th colSpan={alleOrdFraKandidat.length}>
+                                Ord fra kandidaten og relasjon (%)
+                            </th>
+                        </tr>
+                    </thead>
+                    <thead>
+                        <tr>
+                            <th />
+                            {alleOrdFraKandidat.map((ord) => (
+                                <th key={`th-${ord}`}>{ord}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {erfaring.ordScore.map(
+                            ([[, ordFraStilling], matchedeOrdFraKandidaten], i) => {
+                                return (
+                                    <tr key={ordFraStilling}>
+                                        <td>{ordFraStilling}</td>
+                                        {matchedeOrdFraKandidaten.map(([, ord, score]) => (
+                                            <td key={ord}>{tilProsentpoeng(score)}</td>
+                                        ))}
+                                    </tr>
+                                );
+                            }
+                        )}
+                    </tbody>
+                </table>
+            </div>
+            )
+        </details>
+    );
+};
+
+export default Matchdetaljer;
