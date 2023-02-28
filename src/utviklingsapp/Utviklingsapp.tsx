34f8b21034677beeda99765a53741d870c438d44
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-02-28 09:30:25 +0100

    Fiks lenke til CV i utviklingsmodus

--- src/utviklingsapp/Utviklingsapp.tsx
+++ src/utviklingsapp/Utviklingsapp.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, ReactNode, useEffect, useState } from 'react';
 import { createBrowserHistory } from 'history';
 import { Systemtittel } from 'nav-frontend-typografi';
 import { Link } from 'react-router-dom';
@@ -29,39 +29,48 @@ const Utviklingsapp: FunctionComponent = () => {
                 <header className="utviklingsapp">
                     <Systemtittel>Utviklingsapp for rekrutteringsbistand-kandidat</Systemtittel>
 
-                    <div className="utviklingsapp__lenke">
-                        <Link to="/kandidater/lister">Kandidatlister</Link>
-                    </div>
-                    <div className="utviklingsapp__lenke">
-                        <Link to="/prototype/stilling/1ea746af-66be-4cf8-a051-9e815f77b1d1">
-                            Kandidatmatch
-                        </Link>
-                    </div>
-                    <div className="utviklingsapp__lenke">
-                        <Link
-                            state={{
-                                aktørIder: ['PAM010nudgb5v', 'PAM013tc53ryp'],
-                            }}
-                            to="/prototype/stilling/2ea746af-66be-4cf8-a051-9e815f77b1d1"
-                        >
-                            Kandidatmatch (med aktørIder)
-                        </Link>
-                    </div>
-                    <div className="utviklingsapp__lenke">
-                        <Link
-                            to="/kandidater/kandidat/AB123456?fraNyttKandidatsok=true"
-                            state={{
-                                kandidater: ['AB123456', 'BC123456'],
-                            }}
-                        >
-                            Til kandidat
-                        </Link>
-                    </div>
+                    <Utviklingslenke to="/kandidater/lister">Kandidatlister</Utviklingslenke>
+                    <Utviklingslenke
+                        to="/kandidater/kandidat/AB123456/cv?fraNyttKandidatsok=true"
+                        state={{
+                            kandidater: ['AB123456', 'BC123456'],
+                        }}
+                    >
+                        Kandidatside (CV/Historikk)
+                    </Utviklingslenke>
+                    <Utviklingslenke to="/prototype/stilling/1ea746af-66be-4cf8-a051-9e815f77b1d1">
+                        Kandidatmatch
+                    </Utviklingslenke>
+
+                    <Utviklingslenke
+                        state={{
+                            aktørIder: ['PAM010nudgb5v', 'PAM013tc53ryp'],
+                        }}
+                        to="/prototype/stilling/2ea746af-66be-4cf8-a051-9e815f77b1d1"
+                    >
+                        Kandidatmatch (med aktørIder)
+                    </Utviklingslenke>
                 </header>
                 <AppMedStore history={history} navKontor={navKontor} />
             </CustomRouter>
         </div>
     );
 };
+
+const Utviklingslenke = ({
+    to,
+    state,
+    children,
+}: {
+    to: string;
+    state?: object;
+    children: ReactNode;
+}) => (
+    <div className="utviklingsapp__lenke">
+        <Link state={state} to={to}>
+            {children}
+        </Link>
+    </div>
+);
 
 export default Utviklingsapp;
