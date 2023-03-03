1c8900751104e987a979c84a3e2a4080524e1a35
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-03-03 09:11:49 +0100

    Fiks errorsiden slik at den ikke fjerner sin egen feilmelding med én gang

--- src/kandidatliste/Kandidatlisteside.tsx
+++ src/kandidatliste/Kandidatlisteside.tsx
@@ -1,12 +1,12 @@
-import React, { FunctionComponent } from 'react';
-import { useSelector } from 'react-redux';
+import React, { FunctionComponent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Nettstatus } from '../api/Nettressurs';
 import AppState from '../AppState';
 import KandidatlisteOgModaler from './KandidatlisteOgModaler';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 import useScrollTilToppen from './hooks/useScrollTilToppen';
-import useHentKandidatlisteMedId from './hooks/useHentKandidatlisteMedId';
+import KandidatlisteActionType from './reducer/KandidatlisteActionType';
 
 type Props = {
     stillingsId?: string;
@@ -14,10 +14,24 @@ type Props = {
 };
 
 const Kandidatlisteside: FunctionComponent<Props> = ({ stillingsId, kandidatlisteId }) => {
+    const dispatch = useDispatch();
     const { kandidatliste } = useSelector((state: AppState) => state.kandidatliste);
 
     useScrollTilToppen(kandidatliste);
-    useHentKandidatlisteMedId(stillingsId, kandidatlisteId);
+
+    useEffect(() => {
+        if (stillingsId) {
+            dispatch({
+                type: KandidatlisteActionType.HentKandidatlisteMedStillingsId,
+                stillingsId,
+            });
+        } else if (kandidatlisteId) {
+            dispatch({
+                type: KandidatlisteActionType.HentKandidatlisteMedKandidatlisteId,
+                kandidatlisteId,
+            });
+        }
+    }, [dispatch, stillingsId, kandidatlisteId]);
 
     if (kandidatliste.kind === Nettstatus.LasterInn) {
         return (
--- src/kandidatsøk/søkefiltre/error/ErrorSide.js
+++ src/kandidatsøk/søkefiltre/error/ErrorSide.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import ManglerRolle from './ManglerRolle';
 import GenerellFeilside from './GenerellFeilside';
 import NotFound from './NotFound';
 
 const ErrorSide = ({ fjernError, error }) => {
-    const location = useLocation();
+    const { pathname } = useLocation();
+    const forrigePathname = useRef(pathname);
 
     useEffect(() => {
-        fjernError();
-    }, [location, fjernError]);
+        if (forrigePathname.current !== pathname) {
+            fjernError();
+        }
+    }, [pathname, fjernError]);
 
     if (error && error.status === 403) {
         return <ManglerRolle />;
