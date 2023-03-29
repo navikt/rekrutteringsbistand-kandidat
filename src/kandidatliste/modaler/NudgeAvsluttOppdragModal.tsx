c5d2c2b65f0dc80caefb65ec7585122041110b82
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-03-29 08:15:07 +0200

    Fjern overflødig div

--- src/kandidatliste/modaler/NudgeAvsluttOppdragModal.tsx
+++ src/kandidatliste/modaler/NudgeAvsluttOppdragModal.tsx
@@ -38,20 +38,17 @@ const NudgeAvsluttOppdragModal: FunctionComponent<Props> = ({
             aria-label="Forslag om å avslutte oppdraget"
             onClose={onAvbryt}
         >
-            <Heading level="3" size="medium" className={css.tittel}>
+            <Heading spacing level="2" size="medium" className={css.tittel}>
                 Ferdig med oppdraget?
             </Heading>
-
-            <div className={css.beskrivelse}>
-                <BodyShort spacing>
-                    {antallKandidaterSomHarFåttJobb} av {antallStillinger} stilling
-                    {antallStillinger === 1 ? '' : 'er'} er besatt
-                </BodyShort>
-                <BodyShort spacing>Er du ferdig med oppdraget og vil avslutte?</BodyShort>
-            </div>
+            <BodyShort spacing>
+                {antallKandidaterSomHarFåttJobb} av {antallStillinger} stilling
+                {antallStillinger === 1 ? '' : 'er'} er besatt
+            </BodyShort>
+            <BodyShort>Er du ferdig med oppdraget og vil avslutte?</BodyShort>
             <div className={css.knapper}>
                 <Button onClick={onBekreft} className={css.bekreftknapp}>
-                    Ja, Avslutt
+                    Avslutt oppdrag
                 </Button>
                 <Button variant="secondary" onClick={onAvbryt}>
                     Avbryt
