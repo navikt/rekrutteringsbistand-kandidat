66dc45677d6e928dd8b3fe15068b7d05ad6ec51e
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-04-17 10:23:13 +0200

    Erstatt miljøvariabler med variabler

---
+++ src/utils/eksterneUrler.ts
@@ -0,0 +1,11 @@
+import { Miljø, getMiljø } from './miljøUtils';
+
+export const arbeidsrettetOppfølgingUrl =
+    getMiljø() === Miljø.ProdGcp
+        ? 'https://veilarbpersonflate.intern.nav.no'
+        : 'https://veilarbpersonflate.intern.dev.nav.no';
+
+export const lastNedCvUrl =
+    getMiljø() === Miljø.ProdGcp
+        ? 'https://pam-personbruker-veileder.intern.nav.no/cv/pdf?fnr='
+        : 'https://pam-personbruker-veileder.dev.intern.nav.no/cv/pdf?fnr=';
