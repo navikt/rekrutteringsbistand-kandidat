1a84cffa0f83c00447058d39d488c984cffbc76e
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-05-22 13:03:27 +0200

    Ett skritt videre

--- src/kandidatside/fraSøkMedKandidatliste/useNavigerbareKandidaterFraSøk.ts
+++ src/kandidatside/fraSøkMedKandidatliste/useNavigerbareKandidaterFraSøk.ts
@@ -13,17 +13,16 @@ const useNavigerbareKandidaterFraSøk = (
     let index = 0;
     let forrige: string | undefined = undefined;
     let neste: string | undefined = undefined;
-    let antall = 0;
+    let antall = økt?.totaltAntallKandidater ?? 1;
 
-    if (økt?.kandidater === undefined) {
+    if (økt?.kandidaterPåSiden === undefined) {
         return null;
     }
 
-    antall = økt.kandidater.length;
-    index = økt.kandidater.findIndex((kandidat) => kandidat === kandidatnr);
+    index = økt.kandidaterPåSiden.findIndex((kandidat) => kandidat === kandidatnr);
 
-    const forrigeKandidatnr = økt.kandidater[index - 1];
-    const nesteKandidatnr = økt.kandidater[index + 1];
+    const forrigeKandidatnr = økt.kandidaterPåSiden[index - 1];
+    const nesteKandidatnr = økt.kandidaterPåSiden[index + 1];
 
     if (forrigeKandidatnr) {
         forrige = lenkeTilKandidatside(forrigeKandidatnr, fane, kandidatlisteId, false, true);
