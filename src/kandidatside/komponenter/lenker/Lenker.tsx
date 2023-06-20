68e8b13f3b4a8ad71b3c0ef5dbc85f5e0ae90ad8
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-06-20 10:03:02 +0200

    Legg til lenke til 'Finn stilling', ikke i prod

--- src/kandidatside/komponenter/lenker/Lenker.tsx
+++ src/kandidatside/komponenter/lenker/Lenker.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { ExternalLinkIcon, DownloadIcon } from '@navikt/aksel-icons';
-import { Link } from '@navikt/ds-react';
+import { ExternalLinkIcon, DownloadIcon, MagnifyingGlassIcon } from '@navikt/aksel-icons';
+import { Link as NavLink } from '@navikt/ds-react';
+import { Link } from 'react-router-dom';
 
 import { sendEvent } from '../../../amplitude/amplitude';
 import { arbeidsrettetOppfølgingUrl, lastNedCvUrl } from '../../../utils/eksterneUrler';
+import { erIkkeProd } from '../../../utils/featureToggleUtils';
 import css from './Lenker.module.css';
 
 type Props = {
@@ -15,22 +17,31 @@ const Lenker = ({ fødselsnummer, className }: Props) => {
     return (
         <div className={className}>
             <div className={css.lenker}>
-                <Link
+                <NavLink
                     target="_blank"
                     href={`${arbeidsrettetOppfølgingUrl}/${fødselsnummer}`}
                     onClick={() => sendEvent('cv_aktivitetsplan_lenke', 'klikk')}
                 >
                     Se aktivitetsplan
                     <ExternalLinkIcon />
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
                     target="_blank"
                     href={`${lastNedCvUrl}${fødselsnummer}`}
                     onClick={() => sendEvent('cv_last_ned', 'klikk')}
                 >
                     Last ned CV
                     <DownloadIcon />
-                </Link>
+                </NavLink>
+                {erIkkeProd && (
+                    <Link
+                        to={`/stillingssok/${fødselsnummer}`}
+                        className="navds-button navds-button--tertiary navds-button--small"
+                    >
+                        Finn stilling
+                        <MagnifyingGlassIcon />
+                    </Link>
+                )}
             </div>
         </div>
     );
